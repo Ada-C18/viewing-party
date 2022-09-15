2d9b89feebbd2c91282f886862131c1e7d7f1032
Eve Stroud <evestroud89@gmail.com>
2022-09-15 14:46:50 -0400

    Add condition for invalid movies and pass test

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -18,7 +18,6 @@ def test_create_successful_movie():
     assert new_movie["genre"] == GENRE_1
     assert new_movie["rating"] == pytest.approx(RATING_1)
 
-@pytest.mark.skip()
 def test_create_no_title_movie():
     # Arrange
     movie_title = None
@@ -31,7 +30,6 @@ def test_create_no_title_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -44,7 +42,6 @@ def test_create_no_genre_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,8 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    if not title or not genre or not rating:
+        return None
     return {
         "title": title,
         "genre": genre,
