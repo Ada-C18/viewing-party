8a32a6de6c4f8b98190ffa238a445fd69d178b2a
Margaret <y.margaret.lu+github@gmail.com>
2022-09-15 16:41:23 -0700

    solved waves 1 and 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,5 @@
 def create_movie(title, genre, rating):
-    if not title or genre or rating:
+    if not title or not genre or not rating:
         print("Missing title, genre or rating")
         return None
     movie_dict={
@@ -17,22 +17,35 @@ def add_to_watchlist(user_data,movie):
     return user_data
 
 def watch_movie(user_data,title):
-    if title not in user_data["watchlist"].values():
-        return user_data
-    else:
-        user_data["watched"] = user_data["watchlist"].pop(title)
+    for count in range(len(user_data["watchlist"])):
+        if title == user_data["watchlist"][count]["title"]:
+            user_data["watched"].append(user_data["watchlist"].pop(count))
+            break
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-def get_watchedavg_rating(user_data):
-    if not user_data["watched"]["rating"]:
-        return 0.0
-    else:
-        return sum(user_data["watched"]["rating"])/len(user_data["watched"]["rating"])
+import collections
 
+def get_watched_avg_rating(user_data):
+    sum=0.0
+    if len(user_data["watched"])<1:
+        return sum
+    for entry in user_data["watched"]:
+        sum+=entry["rating"]
+    return sum/len(user_data["watched"])
 def get_most_watched_genre(user_data):
-    pass
+    if len(user_data["watched"])<1:
+        return None
+    genre_list=[]
+    for entry in user_data["watched"]:
+        genre_list.append(entry["genre"])
+    counted=collections.Counter(genre_list)
+    return counted.most_common()[0][0]
+
+
+
 
 
 # -----------------------------------------
