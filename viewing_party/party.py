1906a247df432d7cec486603813bf7b47c3cd446
Annie <annie.gallagher7@gmail.com>
2022-09-15 17:04:09 -0600

    completed new functions: create_movie, add_to_watched, add_to_watchlist. created new function: watch_movie.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,32 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    new_movie = {}
+    if title == None or genre == None or rating == None:
+        return None
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+
+    user_data = {
+        "watched": [],
+        "watchlist": [],
+    }
+
+    add_to_watched(user_data, new_movie)
+    add_to_watchlist(user_data, new_movie)
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie):
     pass
 
 # -----------------------------------------
