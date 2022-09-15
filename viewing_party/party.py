b3a935eb9a39f5736d46e842ed9fef517882f80f
allcomputersarebad <allcomputersarebad@users.noreply.github.com>
2022-09-15 14:10:48 -0700

    wave 1 complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,39 @@
 # ------------- WAVE 1 --------------------
 
+
 def create_movie(title, genre, rating):
-    pass
+    return (
+        {"title": title, "genre": genre, "rating": rating}
+        if title and genre and rating
+        else None
+    )
+
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+def get_movie_from_watchlist(user_data, movie_title):
+    try:
+        return next(m for m in user_data["watchlist"] if m["title"] == movie_title)
+    except:
+        return None
+
+
+def watch_movie(user_data, movie_title):
+    movie = get_movie_from_watchlist(user_data, movie_title)
+    if movie:
+        user_data["watchlist"].remove(movie)
+        return add_to_watched(user_data, movie)
+    else:
+        return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -12,7 +44,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -20,4 +52,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
