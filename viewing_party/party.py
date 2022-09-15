ff58afae770e9f4463cfd1f800aad82e68473452
kindra greenawalt <kindra.kathleen@gmail.com>
2022-09-15 18:36:32 -0400

    first attempt to create def create_movie(title,genre,rating) Function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,6 +2,14 @@
 
 def create_movie(title, genre, rating):
     pass
+    #make a dictionary with these keys: "title","genre","rating"
+    movie_dict = {}
+    movie_dict["title"]= title
+    movie_dict["genre"] = genre
+    movie_dict["rating"] = rating
+    return movie_dict
+    
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
