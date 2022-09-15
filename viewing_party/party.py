0204ef4343ec51fc3bdacd6429395bd185a30ce1
Kate <varindakate@gmail.com>
2022-09-15 15:48:42 -0700

    4 tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,16 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    if bool(title) == True and bool(genre) == True and bool(rating) == True:
+        return {
+            "title": title,
+            "genre": genre,
+            "rating": rating
+            }
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
     pass
 
 # -----------------------------------------
