9088562b4a0dc0b4ac3112cb63bac00e55caaaad
Caitlyn <caitlyn.homet@gmail.com>
2022-09-15 19:34:49 -0400

    created watch_movie to pass wave 1 test 7

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,18 +2,15 @@
 
 
 def create_movie(title, genre, rating):
-    
     # return None for incorrect inputs
     if title == None or genre == None or rating == None:
         return None
-
     # create a dictionary with the given input
     new_movie = {
         "title": title,
         "genre": genre,
         "rating": rating
     }
-
     # return the dictionary
     return new_movie
 
@@ -27,6 +24,13 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
+# move movie dict from watchlist to watched list
+def watch_movie(user_data, movie_title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == movie_title:
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+    return user_data
 
 
 
