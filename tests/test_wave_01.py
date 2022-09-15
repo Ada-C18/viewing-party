3b7ef819659c3f9df06cd33805d880838c5e7fa4
June Valentino <vintydesign@gmail.com>
2022-09-15 15:22:41 -0700

    updated wave 1 test function

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -119,7 +119,10 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
+    assert janes_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert janes_data["watched"][0]["genre"] == GENRE_1
+    assert janes_data["watched"][0]["rating"] == RATING_1
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
