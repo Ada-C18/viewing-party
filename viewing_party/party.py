9036dec5e2e8ac7bce2f2f15cfa48a5ab6f62c38
Neida <neidaxmoreno@gmail.com>
2022-09-15 16:53:40 -0400

    created creat_movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,9 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        movie_dict = {"title": title, "genre": genre, "rating": rating}
+        return movie_dict
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
