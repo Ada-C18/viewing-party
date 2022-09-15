8a768457e80b9e1f3b678d1e37cd986e91cbce90
June Valentino <vintydesign@gmail.com>
2022-09-15 14:35:00 -0700

    added a test comment

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,6 +2,7 @@
 
 def create_movie(title, genre, rating):
     pass
+# this is my minimal change code aaaa
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
