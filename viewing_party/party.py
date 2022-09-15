db53be934b34e9811141939b60d62ff2c0131c15
Caitlyn <caitlyn.homet@gmail.com>
2022-09-15 18:38:04 -0400

    Created create_movie, add_to_watched and add_to_watchlist to pass tests 1-6 in wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,34 @@
 # ------------- WAVE 1 --------------------
 
+
 def create_movie(title, genre, rating):
-    pass
+    
+    # return None for incorrect inputs
+    if title == None or genre == None or rating == None:
+        return None
+
+    # create a dictionary with the given input
+    new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+
+    # return the dictionary
+    return new_movie
+
+# add movie dict to watched list
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+# add movie dict to watchlist
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
