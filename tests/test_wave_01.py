9e2e08741eb7459392967b191adb65e72c648c11
Kae Sluder <kaesluder@fastmail.com>
2022-09-15 18:00:43 -0400

    tests and functions for validating input fed to create_movie()

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -4,7 +4,7 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_successful_movie():
     # Arrange
     movie_title = MOVIE_TITLE_1
@@ -19,10 +19,11 @@ def test_create_successful_movie():
     assert new_movie["genre"] == GENRE_1
     assert new_movie["rating"] == pytest.approx(RATING_1)
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_title_movie():
     # Arrange
     movie_title = None
+    # movie_title = ""
     genre = "Horror"
     rating = 3.5
 
@@ -32,7 +33,7 @@ def test_create_no_title_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -45,7 +46,7 @@ def test_create_no_genre_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,44 @@
 # ------------- WAVE 1 --------------------
 
+## WAVE 1.01: validating input
+
+def valid_title(title):
+    if isinstance(title, str) and len(title) > 0:
+        return True
+    else:
+        return False 
+
+def valid_genre(genre):
+    if isinstance(genre, str) and len(genre) > 0:
+        return True
+    else:
+        return False 
+
+def valid_rating(rating):
+    # is there a range? 
+    if isinstance(rating, float):
+        return True
+    else:
+        return False 
+
+
+
 def create_movie(title, genre, rating):
-    pass
+    """create_movie(title, genre, rating) returns a a dict when given:
+    title: string, not empty
+    genre: string, not empty
+    rating: float?"""
+
+    # validate input
+    if valid_title(title) and valid_genre(genre) and valid_rating(rating):
+
+        # model our movies as dictionary objects with title, genre, 
+        # and rating fields 
+        new_movie = {'title': title, 'genre': genre, 'rating': rating}
+        return new_movie 
+
+    else:
+        return None 
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
