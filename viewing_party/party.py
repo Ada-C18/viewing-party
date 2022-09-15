7e6f3007d1aac8afd470645e2895e18a1f07b33e
masinakh <nakhjirimasi@gmail.com>
2022-09-15 14:33:53 -0700

    changed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,7 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    print('Hello world')
+    print('Hello world!')
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
