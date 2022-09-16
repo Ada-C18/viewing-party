de8f66bd20caa55a76730796651cd2ac0b438a74
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-09-16 10:08:55 -0700

    Test wave 01, test 2-5 complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,14 +1,23 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    
-    movie = {
-        "title" : title,
-        "genre" : genre,
-        "rating" : rating,
-    }
+    if title and genre and rating:
+        movie = {
+            "title" : title,
+            "genre" : genre,
+            "rating" : rating,
+        }
+        return movie
 
-    return movie
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
