a8efa4f12fe4f19c565423212ebc37d81aa67740
mhc <mhchau0+ada@gmail.com>
2022-09-16 19:00:25 -0400

    Defined create_movie(), add_to_watched() and add_to_watchlist()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,30 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    #create dictionary to pair "title", "genre", and "rating" to inputted values
+    #if any input element is None, return None
+    
+    new_movie = {}
+    
+    if title and genre and rating:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+        return new_movie
+    
+    return None 
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    
+    return user_data
+
+def watch_movie(user_data, movie_to_watch):
     pass
 
 # -----------------------------------------
