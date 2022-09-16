0eb983071d5d572e7ddec39bafb312bf1a1a6b35
Marilyn Thai <marilynthai15@gmail.com>
2022-09-16 13:08:09 -0700

    Created get most watched genre function and ran and passed all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -32,7 +32,7 @@ def watch_movie(user_data, movie):
 def get_watched_avg_rating(user_watched):
     #putting ratings in a list
     ratings = []
-    if len(user_watched["watched"]) > 0:
+    if user_watched["watched"]:
         for movies in user_watched["watched"]:
             for label,info in movies.items():
                 if label == "rating":
@@ -46,6 +46,20 @@ def get_watched_avg_rating(user_watched):
     average_rating = total / len(ratings)
 
     return average_rating
+
+def get_most_watched_genre(user_watched):
+    genre_popularity = {}
+    if user_watched["watched"]:
+        for movies in user_watched["watched"]:
+                for label,info in movies.items():
+                    if label == "genre":
+                        if info in genre_popularity:
+                            genre_popularity[info] = genre_popularity[info] + 1
+                        else:
+                            genre_popularity[info] = 1
+        most_popular = max(genre_popularity, key=genre_popularity.get)
+        return most_popular
+
 
 
 
