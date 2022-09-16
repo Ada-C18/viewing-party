7807ca99f87934c0b3a8916b22e0b4c4e5248b64
Abby <abbychoi97@gmail.com>
2022-09-15 18:45:27 -0700

    pass test adds movie to user watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,6 +9,18 @@ def create_movie(title, genre, rating):
     movies["rating"] = rating
     return movies
 
+def add_to_watched(user_data, movie):
+    # user_data["watched"] = [list of dictionaries that holds movies the user has watched]
+    # movie will be a dictionary 
+    # add movie to this list 
+    list_of_movies = user_data["watched"]
+    list_of_movies.append(movie)
+    user_data["watched"] = list_of_movies
+    return user_data
+
+# def add_to_watchlist(user_data, movie):
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
