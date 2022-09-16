e6b48903476d20a5669aa13352221ef9b2bd802b
Katherine Abrikian <kabrikian@gmail.com>
2022-09-16 18:45:33 -0400

    wave 1, 6 tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,25 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    
+    movie = {}
+    
+    if not title or not genre or not rating:
+        return None
+    else:    
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+    
+    return movie
+
+def add_to_watched(user_data, movie):
+    user_data['watched'].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data['watchlist'].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
