b761914f169b914aefe48af1e5361a89703cd718
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-09-16 16:56:39 -0700

    Added function watch_movie

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,7 +2,6 @@
 #added a comment to test commit
 from tests.test_constants import MOVIE_TITLE_1
 
-
 def create_movie(title, genre, rating):
     new_movie = {}
     if not title  or not genre or not rating:
@@ -19,6 +18,18 @@ def add_to_watched(user_data, movie):
 
 def add_to_watchlist(user_data, movie):
     user_data  = {"watchlist": [movie]}
+    return user_data
+
+def watch_movie(user_data, title):
+    # initialize variable watch_list that equal dictionary "user_data" with key "watchlist"
+    watch_list = user_data["watchlist"]
+    for movie in watch_list:
+        if movie["title"] == title:
+            # if title of movie from watchlist equal title of watched movie,
+            # than remove that movie from watchlist, return it's value to variable "watched_movie" 
+            # and add to watched list
+            watched_movie = watch_list.pop(watch_list.index(movie))
+            user_data["watched"].append(watched_movie)
     return user_data
 
 
