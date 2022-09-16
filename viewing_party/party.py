a5a923c902bb70670165c891c6f62659957a1c31
Amanda <amanda.scott17@gmail.com>
2022-09-16 15:16:34 -0700

    completed create a movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,28 @@
 # ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
-    pass
+from optparse import TitledHelpFormatter
 
+
+def create_movie(title, genre, rating):
+  my_dict = {}
+  title_value = bool(title)
+  genre_value = bool(genre)
+  rating_value = bool(rating)
+  if title_value and genre_value and rating_value == True:
+    my_dict["title"] = title
+    my_dict["genre"] = genre
+    my_dict["rating"] = rating
+
+  return my_dict
+
+# def add_to_watched(user_data, movie):
+
+
+
+# for 
+
+
+# }
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
@@ -20,4 +40,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
