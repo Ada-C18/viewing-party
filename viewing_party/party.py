a1f52bea50ad4f43322d2c5f03aea932bfd5b5c1
Neida <neidaxmoreno@gmail.com>
2022-09-15 21:27:25 -0400

    added create_movie and add_to_watched functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -4,6 +4,21 @@ def create_movie(title, genre, rating):
     if title and genre and rating:
         movie_dict = {"title": title, "genre": genre, "rating": rating}
         return movie_dict
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    '''
+    Takes two arguments, user_data and movie, where user_data 
+    is a dictionary that stores all of the movies the user has watched 
+    and movie is a dictionary which contains the information for a movie
+    
+    Adds the value of movie to the "watched" list inside of user_data
+
+    Returns user_data
+    '''
+    user_data["watched"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
