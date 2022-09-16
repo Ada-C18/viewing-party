f42bd6211c47017a63cc95804d70d46fe7c4e4b9
Atefeh Mirzaei <atifaayman@gmail.com>
2022-09-16 11:38:55 -0700

    WAVE2 : passed all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -44,6 +44,7 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
 def get_watched_avg_rating(user_data):
     sum = 0
     average = 0
@@ -52,9 +53,51 @@ def get_watched_avg_rating(user_data):
         sum += user_data["watched"][i]["rating"]
         average = sum / length
 
-    return average    
+    return average   
 
 
+def get_most_watched_genre(user_data):
+    genre_dict = {}
+    length = len(user_data["watched"])
+    if length > 0 :
+        for i in range(length):
+            genre = user_data["watched"][i]["genre"]
+            if genre in genre_dict.keys():
+                genre_dict[genre] +=1
+            else :
+                genre_dict[genre] = 1
+
+        # now that I have my genre_dict I can sort
+        #  the list of values and get the last number as the most occoring      
+        # genre_dict_values = genre_dict.values()
+        # genre_dict_keys = genre_dict.keys()
+        genre_list = []
+        for k, v in genre_dict.items():
+            genre_list.append((v, k))
+        genre_list.sort(reverse = True)
+        return genre_list[0][1]
+    else :
+        return None
+
+            
+
+        # sorted_genre_dict_values = list(genre_dict_values).sort(reverse = True)
+        # most_occurring = sorted_genre_dict_values[0]
+
+        # for i in range(len(genre_dict)):
+        """
+            if genre_dict[genre_dict_keys[i]]== most_occurring :
+                return genre_dict_keys[i]
+            else :
+                return None
+
+    else :
+        return None 
+        """             
+
+
+
+    
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
