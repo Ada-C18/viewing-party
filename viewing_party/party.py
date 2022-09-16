cc1349c06279fe08703b20d9df480dd32f45e83b
Abby <abbychoi97@gmail.com>
2022-09-16 13:54:48 -0700

    added get most watched genre function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -40,8 +40,29 @@ def get_watched_avg_rating(user_data):
     if len(ratings) >= 1:
         avg_ratings = sum(ratings)/len(ratings)
         return avg_ratings
-    return 0
+    return 0.0
 
+def get_most_watched_genre(user_data):
+    genres = []
+    if len(user_data["watched"]) >= 1:
+        for movie in user_data["watched"]:
+            genres.append(movie["genre"])
+    
+        genres_set = set(genres)
+        genres_dict = {}
+    
+        for genre in genres_set: 
+            genres_dict[genre] = genres.count(genre)
+        
+        genre_most = 0
+        max_value = 0
+        
+        for key, value in genres_dict.items():
+            if value > max_value: 
+                max_value = value
+                genre_most = key
+        return genre_most
+    return None
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
