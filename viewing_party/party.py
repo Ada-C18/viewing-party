36e4a43ed4aee66453f0120f25a4c88c85e5f08f
Laura Skomerza <lskomerza@gmail.com>
2022-09-16 11:57:28 -0700

    wave 1 test 1 create movie success

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,30 @@
 # ------------- WAVE 1 --------------------
 # this is my first commit 
+
+from operator import ge
+
+
 def create_movie(title, genre, rating):
+    if title or genre or rating:
+        dict = {}
+        dict["title"] = title
+        dict["genre"] = genre
+        dict["rating"] = rating
+        return dict
     
+    
+# 
+# if parameters = True return a dict with 
+# {
+# "title": "Interstellar"
+# "genre": "Drama"
+# "rating": 5}
+# create empty dict
+
+    
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -20,4 +43,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
