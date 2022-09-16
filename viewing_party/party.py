2ec7d500438603da8f0fdf59300540c555bc18a3
Abby <abbychoi97@gmail.com>
2022-09-15 18:51:57 -0700

    add movie to watchlist function made

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,12 +13,16 @@ def add_to_watched(user_data, movie):
     # user_data["watched"] = [list of dictionaries that holds movies the user has watched]
     # movie will be a dictionary 
     # add movie to this list 
-    list_of_movies = user_data["watched"]
-    list_of_movies.append(movie)
-    user_data["watched"] = list_of_movies
+    list_of_movies_watched = user_data["watched"]
+    list_of_movies_watched.append(movie)
+    user_data["watched"] = list_of_movies_watched
     return user_data
 
-# def add_to_watchlist(user_data, movie):
+def add_to_watchlist(user_data, movie):
+    list_of_movies_to_watch = user_data["watchlist"] 
+    list_of_movies_to_watch.append(movie)
+    user_data["watchlist"] = list_of_movies_to_watch
+    return user_data
 
 
 # -----------------------------------------
