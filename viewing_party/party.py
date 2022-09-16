dd040d81b87e8595e853bc097ca81cfebb36c473
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-09-15 23:48:59 -0700

    Added function create_movie

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,17 @@
 # ------------- WAVE 1 --------------------
 #added a comment to test commit
-def create_movie(title, genre, rating):
-    pass
+from tests.test_constants import MOVIE_TITLE_1
 
+
+def create_movie(title, genre, rating):
+    new_movie = {}
+    if not title  or not genre or not rating:
+        return None
+    else:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+    return new_movie
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
