0e558abd86ab4a0e3b63bff22f1705d4a20c9950
Lee Reyes <reyeslee94@gmail.com>
2022-09-16 11:52:53 -0700

    Finished wave_02 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -34,18 +34,40 @@ def watch_movie(user_data,title):
 def get_watched_avg_rating(user_data):
     average = 0
     sum = 0
-    for key in user_data:
-        for i in range(len(user_data[key])):
-            for category in i:
-                if category == "rating":
-                    sum += user_data[key][i][category]
-    average = sum / 2
+    list_of_movies = user_data["watched"]
+
+    if len(list_of_movies) == 0:
+        return 0
+
+    for i in range(len(list_of_movies)):
+        sum += list_of_movies[i]["rating"]
+
+    average = sum / len(list_of_movies)
     return average
+
+
+def get_most_watched_genre(user_data):
+    list_of_movies = user_data["watched"]
+
+    if len(list_of_movies) == 0:
+        return None
+    
+    counter_dict ={}
+    for i in range(len(list_of_movies)):
+        if list_of_movies[i]["genre"] not in counter_dict:
+            counter_dict[list_of_movies[i]["genre"]] = 1
+        else:
+            counter_dict[list_of_movies[i]["genre"]] += 1
+
+    popular_genre = max(counter_dict, key = counter_dict.get)
+    return popular_genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    pass
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
