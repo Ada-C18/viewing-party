3c0d6ab79ddb544b0a7e0202b7dc096c959e9911
Marilyn Thai <marilynthai15@gmail.com>
2022-09-16 12:27:57 -0700

    Created add_to_watch_function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,18 +13,18 @@ def add_to_watched(user_data, movie):
     user_data["watched"] = [movie]
     return user_data 
 
-def add_to_watchlist(user_data, movie):
-    user_data["watchlist"] = [movie]
-    return user_data 
+# def add_to_watchlist(user_data, movie):
+#     user_data["watchlist"] = [movie]
+#     return user_data 
 
-def watch_movie(user_data, movie):
-    for movies in user_data["watchlist"]:
-        for label,info in movies.items():
-            if label == "title":
-                if movie == info:
-                    watched_movie = user_data["watchlist"].pop(user_data["watchlist"].index(movies))
-                    user_data["watched"].append(watched_movie)
-    return user_data
+# def watch_movie(user_data, movie):
+#     for movies in user_data["watchlist"]:
+#         for label,info in movies.items():
+#             if label == "title":
+#                 if movie == info:
+#                     watched_movie = user_data["watchlist"].pop(user_data["watchlist"].index(movies))
+#                     user_data["watched"].append(watched_movie)
+#     return user_data
 
 
 
