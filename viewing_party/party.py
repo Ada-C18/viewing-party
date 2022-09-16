322313345e100e3172886835d66ff8f57c6d3e4b
Steffanie <lopezsteffanie8@gmail.com>
2022-09-16 06:54:18 -0700

    "passed tests 1-5 in wave 01"

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,39 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    
+    new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
 
+    if not title or not genre or not rating:
+        new_movie = None
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    
+    updated_user_data = user_data.copy()
+    updated_user_data["watched"].append(movie)
+
+    return updated_user_data
+
+def add_to_watchlist(user_data, movie):
+
+    updated_user_data = user_data.copy()
+    updated_user_data["watchlist"].append(movie)
+
+    return updated_user_data
+
+def watch_movie(user_data, title):
+
+    updated_user_data = user_data.copy()
+    updated_user_data["watched"].append(updated_user_data["watchlist"].copy())
+    updated_user_data["watchlist"].pop()
+
+    return updated_user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
