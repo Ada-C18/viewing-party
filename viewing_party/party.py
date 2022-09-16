5ac249843d77128f35f5e24f65f43cf3c732a598
wanjunlan <wanjunlan@gmail.com>
2022-09-16 15:50:48 -0700

    implemented the function for wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,34 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if not title or not genre or not rating:
+        return None
+
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data,movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    watch_list = user_data["watchlist"]
+    for i in range(len(watch_list)):
+        if title == watch_list[i]["title"]:
+            user_data["watched"].append(watch_list[i])
+            del watch_list[i]
+    return user_data
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
