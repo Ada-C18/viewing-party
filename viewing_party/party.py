a5e12f643094e4179b6429f3c3e5e9d6174ea9d6
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-09-16 11:50:27 -0700

    updated create_movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,27 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    # check to see if parameters are true
+    # conditional for each paramter 
+    # add parameters to a dictionary if true
+    # create a dictionary
+    # append to dictionary if true, NONE if false
+    # return dictionary
+
+    movie = {}
+    if title == None or genre == None or rating == None:
+        return None
+    else:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        return movie
+
+# def add_to_watched(user_data, movie):
+#     user_data["watched"] = movie
+
+#     return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
