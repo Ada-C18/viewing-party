f4ac52e6fd53e530fd6b4f6f9c52211990ee943e
DESKTOP-USQRFHK\lppas <lp.passarelli.wilson@gmail.com>
2022-09-15 18:01:45 -0700

    passing the first 4 tests of wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,12 +3,13 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    #create a dictionary with the keys 'title', 'genre', 'rating', and the values the values passed in!!
-    movie_dict = {}
-    movie_dict['title'] = title
-    movie_dict['genre'] = genre
-    movie_dict['rating'] = rating
-    return movie_dict
+    #create a dictionary with the keys 'title', 'genre', 'rating', and the values the values passed in
+    if title and genre and rating:
+        movie_dict = {}
+        movie_dict['title'] = title
+        movie_dict['genre'] = genre
+        movie_dict['rating'] = rating
+        return movie_dict
 
 
 # -----------------------------------------
