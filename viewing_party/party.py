938d6f0827aa08023f2ec520b9528ee4fa5b80c1
Abby <abbychoi97@gmail.com>
2022-09-16 11:43:51 -0700

    refactored add to watched and add to watchlist functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,18 +10,11 @@ def create_movie(title, genre, rating):
     return movies
 
 def add_to_watched(user_data, movie):
-    # user_data["watched"] = [list of dictionaries that holds movies the user has watched]
-    # movie will be a dictionary 
-    # add movie to this list 
-    list_of_movies_watched = user_data["watched"]
-    list_of_movies_watched.append(movie)
-    user_data["watched"] = list_of_movies_watched
+    user_data["watched"].append(movie)
     return user_data
 
 def add_to_watchlist(user_data, movie):
-    list_of_movies_to_watch = user_data["watchlist"] 
-    list_of_movies_to_watch.append(movie)
-    user_data["watchlist"] = list_of_movies_to_watch
+    user_data["watchlist"].append(movie)
     return user_data
 
 def watch_movie(user_data, title):
@@ -34,6 +27,7 @@ def watch_movie(user_data, title):
     user_data["watchlist"] = watchlist
     user_data["watched"] = watched
     return user_data
+    
 
 
 # -----------------------------------------
