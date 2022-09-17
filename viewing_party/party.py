24d753c17f3c7dace06a50e09bac0492c3181d41
Isabella <isabella.vanstedum@gmail.com>
2022-09-16 22:33:24 -0300

    Defined new functions, movies user has watched and movie on user to watch

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,23 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
 
+    dict_movie={
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+    for key in dict_movie:
+        if not dict_movie[key]:
+            return None
+    return dict_movie
+def add_to_watched(the_user_data, movie_watched):
+    the_user_data["watched"].append(movie_watched)
+    return the_user_data
+
+def add_to_watchlist(the_user_d, movie_to_watch):
+    the_user_d["watchlist"].append(movie_to_watch)
+    return the_user_d
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
