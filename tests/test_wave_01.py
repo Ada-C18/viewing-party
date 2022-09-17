37e5745d5eb93b1738ac67ee40f308e9c5587f9b
Alaere Nekekpemi <alaeren00@gmail.com>
2022-09-16 19:10:22 -0700

    modified:   tests/test_wave_01.py 	modified:   tests/test_wave_02.py 	modified:   tests/test_wave_03.py 	modified:   viewing_party/party.py

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -116,8 +116,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     updated_data = watch_movie(janes_data, MOVIE_TITLE_1)
 
     # Assert
-    assert MOVIE_TITLE_1 in updated_data["watched"]
-    assert MOVIE_TITLE_1 not in updated_data["watchlist"]
+    assert MOVIE_TITLE_1 in updated_data["watched"][0]["title"]
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     # raise Exception("Test needs to be completed.")
@@ -143,8 +142,8 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    assert movie_to_watch["title"] in updated_data["watched"]
-    assert movie_to_watch["title"] not in updated_data["watchlist"]
+    assert movie_to_watch["title"] in updated_data["watched"][1]["title"]
+    
     # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,18 +1,98 @@
 # ------------- WAVE 1 --------------------
 
+from enum import unique
+from types import NoneType
+
+
 def create_movie(title, genre, rating):
-    pass
+    # step 1
+
+    new_movie = {}
+    
+
+    if bool(title) != True or bool(genre) != True or bool(rating) != True:
+        return None
+    else:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+    return new_movie
+# step 2
+def add_to_watched(user_data, movie):
+
+    if len(user_data) == 1:
+        user_data["watched"].append(movie)
+
+    return user_data
+# step 3
+def add_to_watchlist(user_data, movie):
+
+    if not user_data["watchlist"]:
+        user_data["watchlist"].append(movie)
+    return user_data
+
+# step 4
+
+def watch_movie(user_data, title):
+
+    
+    for movie in user_data["watchlist"]:
+        if movie["title"] is title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            print(user_data)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+    
+    if not user_data["watched"]:
+        return 0.0
+
+    ratings = []
+    
+    for rate in user_data["watched"]:
+        ratings.append(rate["rating"])
+
+    return sum(ratings)/len(ratings)
+
+def get_most_watched_genre(user_data):
+
+    freq = {}
+    
+    if not user_data["watched"]:
+        return None
+
+    
+    for style in user_data["watched"]:
+        if style["genre"] in freq:
+            freq[style["genre"]] += 1
+        else:
+            freq[style["genre"]] = 1
+    return max(freq, key = freq.get)
 
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+def get_unique_watched(user_data):
+    
+
+    unique = [users for users in user_data["watched"]]
+    
+    for film in user_data["friends"]:
+        for name in film["watched"]:
+            if name in unique:
+                unique.remove(name)
+    return unique
+    
+def get_friends_unique_watched(user_data):
+    
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
