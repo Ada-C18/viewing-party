0be9ed115b54f99aa150d10c62930832be5a26ae
fataneh <fkarandish1@gmail.com>
2022-09-16 21:10:32 -0700

    complete wave_1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,36 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        movie_dict = {"title": title,"genre":genre,"rating":rating}
+        return movie_dict
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    user_data = {"watched":[]}
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data = {"watchlist":[]}
+    user_data["watchlist"].append(movie)
+    return user_data
+    
+
+def watch_movie(user_data,title):
+    watched_movie = None
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            watched_movie = movie
+            break
+    if watched_movie:
+        user_data["watchlist"].remove(watched_movie)
+        user_data["watched"].append(watched_movie)
+    return user_data
+
+        
+        
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
