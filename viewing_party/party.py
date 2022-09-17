78c24ae0a993387c38c6f6a8a8922818838690cb
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2022-09-17 12:04:13 -0700

    wave one worked

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,37 @@
 # ------------- WAVE 1 --------------------
 
+
+
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        dict = {}
+        dict["title"] = title
+        dict["genre"] = genre
+        dict["rating"] = rating
+    #   print(dict)
+        return dict
+
+    if not title or not genre or not rating:
+        # print(dict)
+        return None
+
+def add_to_watched(user_data, movie):
+     user_data["watched"].append(movie)
+     return user_data
+  
+    
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            return user_data
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
