bd315d251916fce3db2e3bf1519de473072367a8
Abby <abbychoi97@gmail.com>
2022-09-17 02:59:37 -0700

    added get unique watched function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -44,30 +44,49 @@ def get_watched_avg_rating(user_data):
 
 def get_most_watched_genre(user_data):
     genres = []
-    if len(user_data["watched"]) >= 1:
-        for movie in user_data["watched"]:
-            genres.append(movie["genre"])
+    genre_most_count = 0
+    most_common_genre = None
+
+    for movie in user_data["watched"]:
+        genres.append(movie["genre"])
+        for genre in genres:
+            if genres.count(genre) > genre_most_count:
+                genre_most_count = genres.count(genre)
+                most_common_genre = genre    
     
-        genres_set = set(genres)
-        genres_dict = {}
-    
-        for genre in genres_set: 
-            genres_dict[genre] = genres.count(genre)
-        
-        genre_most = 0
-        max_value = 0
-        
-        for key, value in genres_dict.items():
-            if value > max_value: 
-                max_value = value
-                genre_most = key
-        return genre_most
-    return None
+    return most_common_genre
+
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    user_watched = []
+    friends_watched = []
+    
+    for view_status, movie_list in user_data.items():
+        if view_status == "watched":
+            for movie in movie_list:
+                user_watched.append(movie["title"])
+    for friend_view_status in user_data["friends"]:
+        for movie in friend_view_status["watched"]:
+            friends_watched.append(movie["title"])
+    
+    if len(user_watched) == 0:
+        return user_watched
 
-        
+    user_watched_set = set(user_watched)
+    friends_watched_set = set(friends_watched)
+
+    unique_user_watched_set = user_watched_set - friends_watched_set
+    unique_user_watched_list = list(unique_user_watched_set)
+
+    unique_user_watched = []
+    for movie in user_data["watched"]:
+        if movie["title"] in unique_user_watched_list:
+            unique_user_watched.append(movie)
+
+    return unique_user_watched
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
