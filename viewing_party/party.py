ae7bd1fa71f9adc403eb41c2ab877aa1e528bd84
Liat <liathirsh@gmail.com>
2022-09-17 14:02:56 -0400

    wave 2 - get_watched_avg_rating complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -31,17 +31,26 @@ def watch_movie(user_data, title):
     
     return user_data
 
-            
-    
-    
-
-
-
-
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    total_ratings = 0
+    total_movies = len(user_data['watched'])
+    
+    for index in range(len(user_data["watched"])):
+        total_ratings+= user_data["watched"][index]["rating"]
+
+    if total_ratings == 0:
+        return 0.0
+
+    average = total_ratings/total_movies
+    
+    return average
+
+def get_most_watched_genre(user_data):
+    if len
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
