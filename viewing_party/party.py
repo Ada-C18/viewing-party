3f8a9dec9fb71e80c3b1f2493396c3bbf66fdc3b
Liat <liathirsh@gmail.com>
2022-09-16 21:17:32 -0400

    Created a working create movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,27 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    
+    movie = {}
+
+    if title == None or genre == None or rating == None:
+        return None
+    else:
+        movie['title'] = title
+        movie['genre'] = genre
+        movie['rating'] = rating
+        
+    return movie
+
+#def add_to_watched(user_data, movie):
+    user_data["watched"] = movie   
+
+    return user_data
+
+#def add_to_watchlist(user_data, movie):
+    user_data["watchlist"] = movie
+
+#def watch_movie()
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
