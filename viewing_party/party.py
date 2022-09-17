7c647b61ec50224250ab7301bafe8639686aa8b8
Isabella <isabella.vanstedum@gmail.com>
2022-09-17 12:49:36 -0300

    executed and added to all tests in wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,6 +18,17 @@ def add_to_watched(the_user_data, movie_watched):
 def add_to_watchlist(the_user_d, movie_to_watch):
     the_user_d["watchlist"].append(movie_to_watch)
     return the_user_d
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        for detail in movie:
+            if detail == "title":
+                if movie["title"] == title:
+                    user_data["watched"].append(movie)
+                    user_data["watchlist"].pop(user_data["watchlist"].index(movie))
+                    return user_data
+    else:
+        return user_data
+        
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
