6ae46ae9bf2df429a359a0fe13f0b957b1ed8d6e
Milena Souto <yamadasouto@gmail.com>
2022-09-17 11:18:15 -0700

    Implement add_to_watched to pass test_adds_movie_to_user_watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,11 +1,5 @@
 # ------------- WAVE 1 --------------------
-# 1. Create a function named  `create_movie`. This function and all subsequent functions should be in `party.py`. `create_movie` should...
-# - take three parameters: `title`, `genre`, `rating`
-# - If those three attributes are truthy, then return a dictionary. This dictionary should...
-#   - Have three key-value pairs, with specific keys
-#   - The three keys should be `"title"`, `"genre"`, and `"rating"`
-#   - The values of these key-value pairs should be appropriate values
-# - If `title` is falsy, `genre` is falsy, or `rating` is falsy, this function should return `None`
+# 1. Create a function named  `create_movie`. This function and all subsequent functions should be in `party.py`. 
 
 def create_movie(title, genre, rating):
     if title and genre and rating:
@@ -18,20 +12,9 @@ def create_movie(title, genre, rating):
 
 # 2. Create a function named `add_to_watched`. This function should...
 
-# - take two parameters: `user_data`, `movie`
-#   - the value of `user_data` will be a dictionary with a key `"watched"`, and a value which is a list of dictionaries representing the movies the user has watched
-#     - An empty list represents that the user has no movies in their watched list
-#   - the value of `movie` will be a dictionary in this format:
-#     - ```python
-#       {
-#         "title": "Title A",
-#         "genre": "Horror",
-#         "rating": 3.5
-#       }
-#       ```
-# - add the `movie` to the `"watched"` list inside of `user_data`
-# - return the `user_data`
-
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
 
 # -----------------------------------------
