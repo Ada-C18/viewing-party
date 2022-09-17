f01de5372e01863cbb250cc8d88f7a932d77a2e7
Katherine Abrikian <kabrikian@gmail.com>
2022-09-17 08:10:19 -0400

    Passed all wave 2 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -47,6 +47,43 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    total_rating = 0.0
+    avg_rating = 0.0
+    watched = user_data['watched']
+
+    if len(watched):
+        for movie in watched:
+            total_rating += movie['rating']
+        
+        avg_rating = total_rating / len(watched)
+    
+    return avg_rating
+
+def get_most_watched_genre(user_data):
+    watched = user_data['watched']
+    genre_names = set()
+    genre_dict = {}
+
+    # because we don't know the genres that the user has watched, \
+    # we have to determine what these are and initialize the dictionary \
+    # before we can increment the genres.
+
+    if len(watched):
+        for movie in watched:
+            genre_names.add(movie['genre'])
+        
+        for genre in genre_names:
+            genre_dict[genre] = 0
+        
+        for movie in watched:
+                for genre in genre_names:
+                    if movie['genre'] == genre:
+                        genre_dict[genre] += 1
+        return max(genre_dict, key = genre_dict.get)
+
+    else:
+        return None
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
