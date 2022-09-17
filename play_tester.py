19d2cdabefc299b8ff6acd22e7fd13439451e7ce
Lisa <elizabethpham726@gmail.com>
2022-09-17 15:12:04 -0700

    Finished Wave 1

--- play_tester.py
+++ play_tester.py
@@ -10,9 +10,24 @@ pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
 print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
+movie_to_watch = HORROR_1
+janes_data = {
+        "watchlist": [
+            FANTASY_1,
+            movie_to_watch
+        ],
+        "watched": [FANTASY_2]
+    }
+print(janes_data["watchlist"][0]['title'])
+
+    # Act
+updated_data = watch_movie(janes_data, movie_to_watch["title"])
+print(updated_data)
+
+
 
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,13 +18,14 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
-# create funct "watch_movie"
-# take in 2 parameters:
-# user_data: dict of dictionaries "watchlist" and "watched"
-# title: string as title of movie
 def watch_movie(user_data, title):
+    for i in range(len(user_data["watchlist"])):
+        if title in user_data["watchlist"][i]["title"]:
+            user_data["watched"].append(user_data["watchlist"][i])
+            user_data["watchlist"].remove(user_data["watchlist"][i])
+    return user_data
     
-    pass
+    
 # if title in watchlist
 # remove movie from watchlist
 # add to watched
@@ -36,17 +37,32 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    pass
 
+def get_most_watched_genre(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    pass
 
-        
+def get_friends_unique_watched(user_data):
+    pass
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
-    # ----------------------------------------- 
+# ----------------------------------------- 
+def get_new_rec_by_genre(user_data):
+    pass
+
+def get_rec_from_favorites(user_data):
+    pass
