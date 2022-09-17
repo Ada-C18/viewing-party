f09a61ea3c44ea6160eda9f97c9e20abd449b7f7
Tara <talsaidi@ucdavis.edu>
2022-09-17 09:07:12 -0700

    initialized additional three functions, passed 4/9

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,9 +9,26 @@ def create_movie(title, genre, rating):
     new_movie["genre"] = genre
     new_movie["rating"] = rating
 
-    print(new_movie)
     return new_movie
 
+def add_to_watched(user_data, movie):
+    '''
+    user data is a dictionary containing a key "watched" with value []
+    '''
+    pass
+
+def add_to_watchlist(user_data, movie):
+    '''
+    user data is dictionary containing value "watchlist with value []
+    '''
+    pass
+
+def watch_movie(user_data, MOVIE_TITLE_1):
+    '''
+    moves movie from user data dictionary w/ watchlist list using movie title -->
+    watched list key in same dictionary (an empty list) 
+    '''
+    pass
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
