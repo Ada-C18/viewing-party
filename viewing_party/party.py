c26c43b36b75d10d7a7204aac88677be78a60f8e
Presley <persleytieman@outlook.com>
2022-09-16 22:19:00 -0400

    finished first problem in test wave 01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,13 +1,25 @@
 # ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
-    pass
+import re
+from shutil import move
+from tests.test_constants import GENRE_1, MOVIE_TITLE_1, RATING_1
 
+
+def create_movie(title, genre, rating):
+    new_movie = {"title": [], "genre": [], "rating": []}
+    new_movie["title"] = MOVIE_TITLE_1
+    new_movie["genre"] = GENRE_1
+    new_movie["rating"] = RATING_1
+    if not title or not genre or not rating:
+        return None
+    else:
+        return new_movie
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
-
+# def add_to_watched(user_data, movie):
+    # user_data = {"watched" : }
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
@@ -20,4 +32,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
