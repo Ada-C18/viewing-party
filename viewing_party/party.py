caf98b4dba150189566b619fb1b8aab84586b942
Soleil <soleilherring@gmail.com>
2022-09-18 16:36:02 -0700

    Function Wave 04: Created function to find movie recs that match subscription services and are unseen by user

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -78,8 +78,9 @@ def get_unique_watched(user_data):
 def get_friends_unique_watched(user_data):
     friend_list_movie =[]
     user_list_movie =[]
-    friend_unique_list = []
-    
+    friend_unique_list=[]
+
+
     for dict in user_data["friends"]:
         for movie in dict["watched"]:
             friend_list_movie.append(movie)
@@ -101,17 +102,17 @@ def get_friends_unique_watched(user_data):
     return friend_no_duplicates
             
 
-    
-
-
-
-
-
-
-
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
+    movie_with_subscriptions =[]
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["host"] in user_data["subscriptions"] and movie not in user_data["watched"]:
+                movie_with_subscriptions.append(movie)
+    return movie_with_subscriptions
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
