b4e3f78e369521932aae799d430d00a8006ba397
Elsje Slothower <elsje.slothower@gmail.com>
2022-09-17 23:59:11 -0700

    completed wave 02 - refine get_most_watched_genre function at a later date

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -46,6 +46,40 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    rating = 0
+    for movie in user_data["watched"]:
+        rating += movie["rating"]
+    if rating == 0:
+        return 0
+    else:
+        avg_rating = rating / len(user_data["watched"])
+        return avg_rating
+
+def get_most_watched_genre(user_data):
+    if len(user_data["watched"]) == 0:
+        return None
+    horror_counter = 0
+    fantasy_counter = 0
+    action_counter = 0
+    intrigue_counter = 0
+    for movie in user_data["watched"]:
+        if movie["genre"] == "Horror":
+            horror_counter += 1
+        elif movie["genre"] == "Fantasy":
+            fantasy_counter += 1
+        elif movie["genre"] == "Action":
+            action_counter += 1
+        elif movie["genre"] == "Intrigue":
+            intrigue_counter += 1
+    if horror_counter > fantasy_counter and horror_counter > action_counter and horror_counter > intrigue_counter:
+        return "Horror"
+    if fantasy_counter > horror_counter and fantasy_counter > action_counter and fantasy_counter > intrigue_counter:
+        return "Fantasy"
+    if action_counter > horror_counter and action_counter > fantasy_counter and action_counter > intrigue_counter:
+        return "Action"
+    if intrigue_counter > horror_counter and intrigue_counter > fantasy_counter and intrigue_counter > action_counter:
+        return "Intrigue"
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
