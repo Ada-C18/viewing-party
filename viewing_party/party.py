db902b727a91015a613f0b826d3a50a9d280a793
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-18 16:08:06 -0400

    wave 3 function 1, unique user list complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -88,11 +88,40 @@ def get_most_watched_genre(user_data):
 
     # Function compares user watched list with friends watched, Returns UNIQUE list ONLY USER watched.
 
-    # def get_unique_watched(user_data):
 
+def get_unique_watched(user_data):
+    user_watched_list = user_data['watched']
+    complete_list_of_friends = user_data['friends']
+    user_unique_movie_list = []
+    friends_title_list = []
+    for friend in complete_list_of_friends:
+        for friend_dict in friend['watched']:
+            friends_title_list.append(friend_dict['title'])
+    for user_dict in user_watched_list:
+        if user_dict['title'] not in friends_title_list:
+            user_unique_movie_list.append(user_dict)
+    return user_unique_movie_list
+
+ # user_title_list.append(user_dict['title'])
+# friends_title_list.append(friend_dict['title'])
+ # for user_dict in user_watched_list:
+    # if friend_dict in user_watched_list or friend_dict['title'] == user_dict['title']:
+    #     continue
+    # if user_dict not in user_unique_movie_list:
+    # unified_friends_set = friend_1_watched | friend_2_watched
+    # users_unique_movies = user_watched_list | unified_friends_set
+    # turn into dictionary user_unique_movies
+    # return users_unique_movies
+
+
+# -----------------------------------------------
     # Function that compares user list with friends, returns UNIQUE list ONLY FRIENDS watched
-
-    # def get_friends_unique_watched(user_data):
+# def get_friends_unique_watched(user_data):
+#     user_watched_set = user_data['watched']
+#     friend_1_watched = user_data['friends']
+#     friend_2_watched = user_data['friends'][1]
+#     # return friends_unique_movies
+#     pass
 
     # -----------------------------------------
     # ------------- WAVE 4 --------------------
