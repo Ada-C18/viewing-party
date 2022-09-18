f1bdfa9e4f1d0cfe28469a4f652d3cc74c9c2c1d
Elsje Slothower <elsje.slothower@gmail.com>
2022-09-17 23:37:55 -0700

    completed wave 01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -25,17 +25,21 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 # finds the title of a movie in watchlist, then removes and transfers it to watched
+
 def watch_movie(user_data, title):
+    counter = 0
     for movie in user_data["watchlist"]:
-        if title in movie:
-            user_data["watched"] = movie
-            del(user_data["watchlist"][movie])
+        if title in movie["title"]:
+            user_data["watched"].append(movie)
+            del(user_data["watchlist"][counter])
         # if title in user_data["watchlist"]:
             # how to remove the title from the watchlist
             # movie_seen = user_data["watchlist"].pop(title)
             # movie_seen = user_data.pop("watchlist")
             # user_data["watched"][movie] = [movie_seen]
             return user_data
+        counter += 1  
+    return user_data     
 
 
 # -----------------------------------------
