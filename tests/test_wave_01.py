5ea75d3c3eb891e5ec10686140869cf7ed3e632d
mhc <mhchau0+ada@gmail.com>
2022-09-18 12:53:29 -0400

    finished watch_movie tests (completed wave_01.py)

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -120,7 +120,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watched"]) == 1
     assert janes_data["watchlist"][0] == updated_data["watched"][0]
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -138,13 +138,9 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert movie_to_watch in updated_data["watched"]
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
