f8db05c3424266524e3fc1dab677a3b1543890ec
Aretta Brown <aretta.brown@gmail.com>
2022-09-18 00:39:29 -0400

    defines add to watchlist

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,6 +13,10 @@ def create_movie(title, genre, rating):
 def add_to_watched(data, movie):
     data["watched"].append(movie)
     return data
+
+def add_to_watchlist(data, movie):
+    data["watchlist"].append(movie)
+    return data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
