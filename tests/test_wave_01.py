a6e5fbe175ac2006ae809a92c9bf5b29476d34f0
Erika Sha <erikaasha05@gmail.com>
2022-09-18 16:51:45 -0400

    Updated test 8 in test wave 1 to add an assertion to test that the correct movie was added to watched

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -130,7 +130,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
             "rating": RATING_1
         }]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -149,10 +149,14 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
+    assert updated_data["watched"] == [
+        FANTASY_2, 
+        movie_to_watch
+    ]
 
 @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
