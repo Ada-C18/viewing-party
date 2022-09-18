edd8ed4468aaa538875fea1adab198661c9a1523
Maxwell <maxpasqualis@gmail.com>
2022-09-18 15:19:38 -0700

    Implemented genre recommendations, adjusted variable names

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -82,9 +82,9 @@ def get_unique_watched(user_data):
 
 def get_friends_unique_watched(user_data):
     unique_friends_watched = []
-    all_friends_watched = get_friends_watched(user_data)
+    friends_watched = get_friends_watched(user_data)
     # Compares friends' watched list to user's watched list
-    for movie in all_friends_watched:
+    for movie in friends_watched:
         if movie not in user_data["watched"]:
             unique_friends_watched.append(movie)
     
@@ -96,8 +96,8 @@ def get_friends_unique_watched(user_data):
 
 def get_available_recs(user_data):
     recs = []
-    all_friends_watched = get_friends_watched(user_data)
-    for movie in all_friends_watched:
+    friends_watched = get_friends_watched(user_data)
+    for movie in friends_watched:
         if movie["host"] in user_data["subscriptions"]:
             if movie not in user_data["watched"]:
                 recs.append(movie)
@@ -107,3 +107,12 @@ def get_available_recs(user_data):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+def get_new_rec_by_genre(user_data):
+    recs = []
+    users_fave_genre = get_most_watched_genre(user_data)
+    friends_watched = get_friends_watched(user_data)
+    for movie in friends_watched:
+        if movie["genre"] == users_fave_genre:
+            if movie not in user_data["watched"]:
+                recs.append(movie)
+    return recs
