937163c2b0d1fb6471117b33d8953ae91886a4ee
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-18 07:18:16 -0400

    finished Wave 2

--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -17,7 +17,7 @@ def test_calculates_watched_average_rating():
     assert janes_data == clean_wave_2_data()
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_empty_watched_average_rating_is_zero():
     # Arrange
     janes_data = {
@@ -31,7 +31,7 @@ def test_empty_watched_average_rating_is_zero():
     assert average == pytest.approx(0.0)
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_most_watched_genre():
     # Arrange
     janes_data = clean_wave_2_data()
@@ -44,7 +44,7 @@ def test_most_watched_genre():
     assert janes_data == clean_wave_2_data()
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_genre_is_None_if_empty_watched():
     # Arrange
     janes_data = {
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -32,9 +32,6 @@ def add_to_watchlist(user_data, movie):
     user_data = {"watchlist": [movie]}
     return user_data
 
-# pop watchlist in list-dict-List title
-# store in variable ,append to watched list
-
 
 def watch_movie(user_data, title):
     if user_data["watchlist"]:
@@ -49,13 +46,11 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
-# {'watched': [{'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, {'title': 'The Lord of the Functions: The Two Parameters', 'genre': 'Fantasy', 'rating': 4.0}, {'title': 'The Lord of the Functions: The Return of the Value', 'genre': 'Fantasy', 'rating': 4.0}, {'title': 'The JavaScript and the React', 'genre': 'Action', 'rating': 2.2}, {'title': 'Recursion', 'genre': 'Intrigue', 'rating': 2.0}, {'title': 'Instructor Student TA Manager', 'genre': 'Intrigue', 'rating': 4.5}]}
-
 
 def get_watched_avg_rating(user_data):
     sum = 0
     ratings_list = []
-    if user_data:
+    if user_data['watched']:
         for i in user_data["watched"]:
             ratings_list.append(i['rating'])
         list_len = len(ratings_list)
@@ -63,38 +58,50 @@ def get_watched_avg_rating(user_data):
             sum += i
         average = sum/list_len
         return average
-# empty watched list has 0.0 average rating
-
     return 0.0
 
 
-# def get_most_watched_genre(user_data): #param same as above
-    # use genre keys in each movie to find which is most watched.
-    # if value of user_data is empty, return None
+def get_most_watched_genre(user_data):
+    genre_list = []
+    genre_tally_dict = {}
+    most_watched_str = None
+    most_watched_int = 0
+    if user_data:
+        for i in user_data["watched"]:
+            genre_list.append(i['genre'])
+        for i in genre_list:
+            if i not in genre_tally_dict:
+                genre_tally_dict[i] = 1
+                most_watched_int = 1
+            else:
+                genre_tally_dict[i] += 1
+                most_watched_int += 1
+            for i in genre_tally_dict:
+                if genre_tally_dict[i] > most_watched_int:
+                    most_watched_str = i
+        return most_watched_str
+        # use genre keys in each movie to find which is most watched.
+    return None
+    # -----------------------------------------
+    # ------------- WAVE 3 --------------------
+    # -----------------------------------------
 
+    # Function compares user watched list with friends watched, Returns UNIQUE list ONLY USER watched.
 
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
+    # def get_unique_watched(user_data):
 
-# Function compares user watched list with friends watched, Returns UNIQUE list ONLY USER watched.
+    # Function that compares user list with friends, returns UNIQUE list ONLY FRIENDS watched
 
-# def get_unique_watched(user_data):
+    # def get_friends_unique_watched(user_data):
 
-# Function that compares user list with friends, returns UNIQUE list ONLY FRIENDS watched
+    # -----------------------------------------
+    # ------------- WAVE 4 --------------------
+    # -----------------------------------------
 
-# def get_friends_unique_watched(user_data):
+    # def get_available_recs(user_data):
 
+    # -----------------------------------------
+    # ------------- WAVE 5 --------------------
+    # -----------------------------------------
 
-# -----------------------------------------
-# ------------- WAVE 4 --------------------
-# -----------------------------------------
-
-# def get_available_recs(user_data):
-
-
-# -----------------------------------------
-# ------------- WAVE 5 --------------------
-# -----------------------------------------
-
-# def get_new_rec_by_genre(user_data):
+    # def get_new_rec_by_genre(user_data):
