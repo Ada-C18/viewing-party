e5248e473c3d7b9cb39c148a66b47ce6f95dab42
kimiahasani <kn.improve@gmail.com>
2022-09-18 11:41:04 -0700

    wave2 passed all the test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -32,6 +32,39 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    average_rating = 0
+    sum = 0
+    lenght = len(user_data["watched"])
+    for i in range(lenght):
+        sum += user_data["watched"][i]["rating"]
+        average_rating = sum/lenght 
+    return average_rating
+
+def get_most_watched_genre(user_data):
+    genre_movie ={}
+    lenght = len(user_data["watched"])
+    if lenght == 0:
+        return None
+    else:  
+        for i in range(lenght):
+            genre = user_data["watched"][i]["genre"]
+            if genre in genre_movie:
+                genre_movie[genre] +=1
+            else:
+                genre_movie[genre]=1
+
+        genre_movie_list =[]
+        for k, v in genre_movie.items():
+            genre_movie_list.append((v,k))
+        genre_movie_list.sort()
+        return genre_movie_list[-1][1]
+
+
+
+
+
+      
 
 
 # -----------------------------------------
