4a4013edd685f695805483420bc61b4937bffda5
Mia Yang <miacxyang@gmail.com>
2022-09-17 21:47:55 -0700

    "Wave_01 last 3 tests needs recheck"

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,43 @@
 # ------------- WAVE 1 --------------------
 
+from optparse import TitledHelpFormatter
+
+
 def create_movie(title, genre, rating):
-    pass
+    
+    new_movie = {}
+
+    if title and genre and rating:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+    else:
+        return None
+    
+    return new_movie
+
+
+def add_to_watched(user_data, movie):
+
+    user_data["watched"].append(movie)
+
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+
+def watch_movie(user_data, title):
+    for i in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][i]["title"] == title:
+            user_data["watched"].append(user_data["watchlist"][i])
+            user_data["watchlist"].pop(i)
+            
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
