8650736d621dd5000665c45e873c915306e9368f
Intesar Siraj <ghideruk@gmail.com>
2022-09-17 18:30:50 -0700

    wave 2 test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,22 +3,26 @@
 #funtion with three parameters
 def create_movie(title, genre, rating):
     if title and genre and rating:
-        dict={}
+        dict = {}
         dict["title"] = title
         dict["genre"] = genre
         dict["rating"] = rating
+    #   print(dict)
         return dict
+
     if not title or not genre or not rating:
+        # print(dict)
         return None
 
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
 
+    
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
-    
+
 
 def watch_movie(user_data, title):
     for movie in user_data["watchlist"]:
@@ -28,23 +32,29 @@ def watch_movie(user_data, title):
             return user_data
     return user_data
 
-    
+# ------------- WAVE 2 --------------------
 
-def get_most_watched_avg_rating(user_data):
+def get_watched_avg_rating(user_data):
     sum = 0
     average = 0
-   
+
     for i in range(len(user_data["watched"])):
         sum += user_data["watched"][i]["rating"]
-        average = sum / len(user_data["watched"])
-    return average 
+        average = sum  / len(user_data["watched"])
+
+    return average
 
 def get_most_watched_genre(user_data):
+
+    # if user_data == {"watched": []}:
+    #     return None
+
+    # else:
     index = 0
     genres = []
     least_popular = []
     most_popular = []
-
+    
     if user_data == {"watched":[]}:
         return None
     else:
@@ -57,9 +67,6 @@ def get_most_watched_genre(user_data):
             else:
                 most_popular.append(item)
         return (most_popular[0])
-        
-    
-
 
 
 
