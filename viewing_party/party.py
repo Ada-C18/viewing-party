0edc7cc14633629bda4061ed59b20de1930e2fc0
Erika Sha <erikaasha05@gmail.com>
2022-09-18 13:42:01 -0400

    Updated create_movie function to pass tests 1-4, created add_to_watched function to pass test 5, created add_to_watchlist function to pass test 6.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,13 +1,35 @@
 # ------------- WAVE 1 --------------------
-
+# Create create_movie function that adds movies (title, genre, rating) to a dictionary
 def create_movie(title, genre, rating):
     movie_dict = {}
     movie_dict["title"] = title
     movie_dict["genre"] = genre
     movie_dict["rating"] = rating
     
-    return movie_dict
+    for category in movie_dict:
+        if movie_dict[category] == None:
+            return None
+    
     print(movie_dict)
+    return movie_dict
+
+# Create add_to_watched function that adds movies that a user has watched to a dict
+def add_to_watched(user_data, movie):
+    watched_list = []
+    watched_list.append(movie)
+    user_data["watched"] = watched_list
+
+    print(user_data)
+    return user_data
+
+# Create add_to_watchlist function that adds movies to a user's watchlist in a dict
+def add_to_watchlist(user_data, movie):
+    watchlist = []
+    watchlist.append(movie)
+    user_data["watchlist"] = watchlist
+
+    print(user_data)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
