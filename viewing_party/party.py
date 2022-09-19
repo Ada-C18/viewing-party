e83b946cb06ba53b39b2eae9648677fc936219f3
Dominique <dominiquedaba@gmail.com>
2022-09-19 15:45:04 -0700

    add_to_watched func passed test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -9,13 +9,20 @@ def create_movie(title, genre, rating):
         return new_movie
     return None
 
-# def add_to_watched(user_data, movie):
+def add_to_watched(user_data, movie):
+    # add movie to the "watched" list inside "user_data"
+    # "watched" is a dictionary key but it's also the index
+    # of the list "user_data"
+    user_data["watched"].append(movie)
+    return user_data
 
-
+def add_to_watchlist(user_data, movie):
+    
 # -----------------------------------------
+
 # ------------- WAVE 2 --------------------
-# -----------------------------------------
 
+# -----------------------------------------
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
