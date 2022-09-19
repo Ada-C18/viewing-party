659abdc940ecd847e3104ebb10a123eb3c8e4bac
Aretta Brown <aretta.brown@gmail.com>
2022-09-19 01:59:17 -0400

    adds assert to 8th test

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -118,13 +118,10 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    #raise Exception("Test needs to be completed.")
-
     assert [{'genre': 'Horror', 'rating': 3.5, 'title': 'It Came from the Stack Trace'}] == updated_data["watched"]
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -142,13 +139,9 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
+    assert movie_to_watch in updated_data["watched"]
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
-
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
