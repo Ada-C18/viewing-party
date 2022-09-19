59545780eab22154721d0c293d1243ba45ad4d83
Valentina Ruiz <v.zruiz06@gmail.com>
2022-09-19 14:16:02 -0700

    Completed 5 waves

--- play_tester.py
+++ play_tester.py
@@ -9,21 +9,40 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
-print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# print("\n-----Wave 01 test data-----")
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
+
+# print(create_movie("title1", "genre1", "rathing1"))
+# print(add_to_watched("stacy", )
+# janes_data = {
+#     "watchlist": [{
+#         "title": MOVIE_TITLE_1,
+#         "genre": GENRE_1,
+#         "rating": RATING_1
+#     }],
+#     "watched": []
+# }
+# watch_movie(janes_data, MOVIE_TITLE_1)
+
+# {"title":"Shrek", "genre": "Family", "rating": 4.5})
 
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
+# print(get_most_watched_genre(clean_wave_2_data))
 
-#print("\n-----Wave 03 user_data-----")
+# print("\n-----Wave 03 user_data-----")
 #pp.pprint(clean_wave_3_data())
+# get_unique_watched(clean_wave_3_data())
+# print(get_friends_unique_watched(clean_wave_3_data()))
 
 # Wave 04 user data
-#print("\n-----Wave 04 user_data-----")
+# print("\n-----Wave 04 user_data-----")
 #pp.pprint(clean_wave_4_data())
+# print(get_available_recs(clean_wave_4_data()))
 
 # Wave 05 user data
-#print("\n-----Wave 05 user_data-----")
+print("\n-----Wave 05 user_data-----")
 #pp.pprint(clean_wave_5_data())
+print(get_rec_from_favorites(clean_wave_5_data()))
