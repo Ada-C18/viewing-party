1d41d2f5dbe59f1cec8332bc0aeb05f984a556e2
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-09-18 17:24:59 -0700

    Test Wave 2 complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -39,6 +39,33 @@ def watch_movie(user_data, movie_title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+#Function to calculate the average rating of a users watched movies
+def get_watched_avg_rating(user_data):
+    average = 0
+
+    for movie in range(len(user_data["watched"])):
+        average += user_data["watched"][movie]["rating"]
+
+    # Return 0 if the user has no movies in watched
+    if len(user_data["watched"]) > 0:  
+        average /= len(user_data["watched"])
+
+    return average
+
+#function to calculate the most popular genre from watched
+def get_most_watched_genre(user_data):
+
+    genre_count = {}
+
+    for movie in range(len(user_data["watched"])):
+        if not user_data["watched"][movie]["genre"] in genre_count:
+            genre_count[user_data["watched"][movie]["genre"]] = 1
+        else:
+            genre_count[user_data["watched"][movie]["genre"]] += 1
+
+    if user_data["watched"]:
+        return max(genre_count, key = genre_count.get)
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
