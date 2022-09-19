f16aa945ef355082ff66fca28e2356f4a3cbc242
kindra greenawalt <kindra.kathleen@gmail.com>
2022-09-19 13:32:24 -0400

    wave 1 functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,9 +7,30 @@ def create_movie(title, genre, rating):
     movie_dict["title"]= title
     movie_dict["genre"] = genre
     movie_dict["rating"] = rating
-    return movie_dict
     
+    information = [title,genre,rating]
+    if None in information:
+        return None
+    else:
+        return movie_dict
 
+def add_to_watched(user_data, movie):
+    user_data["watched"] = [movie]
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"] = [movie]
+    return user_data 
+
+def watch_movie(user_data,title):
+    watched_movie = title
+    for movie in user_data["watchlist"]:
+        for value in movie.values():
+            if value == watched_movie:
+                user_data["watchlist"].remove(movie)
+                user_data["watched"].append(movie)
+
+    return(user_data)
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
