70d58c7b245455a33f72933db0ea3ff03bf46885
Tia Willis <tiawillis1@icloud.com>
2022-09-18 20:28:57 -0400

    Wave 1 complete, functions created: create_movie, add_to_wtached, add_to_watchlist, and watch_movie. All tests in wave 1 pass.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,32 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    #pass
+    if title and genre and rating:
+        new_movie = {"title": title , "genre": genre, "rating": rating}
+        return new_movie
+    else: 
+        return None 
+
+def add_to_watched(user_data, movie):
+
+    user_data.get("watched").append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+
+    user_data.get("watchlist").append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+
+    for movie in user_data.get("watchlist"):
+        if movie["title"] == title:
+            user_data.get("watchlist").remove(movie)
+            user_data.get("watched").append(movie)
+            return user_data
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
