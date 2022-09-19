c88aae945ac716b6a4ddebd7e323b8d67f006f7e
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-09-19 15:03:32 -0700

    removed test print statement

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -72,13 +72,8 @@ def get_most_watched_genre(user_data):
             genre_count[i] = count + 1
     
     max_genre = max(genre_count, key =genre_count.get)
+    print(genre_count)
     return max_genre
-
-            
-
-    # empty list
-    # count genres
-    # find max
 
 
 # -----------------------------------------
