5ae5373c0d18026660e887adef8e79b699b5abc9
annalord <annalordkemp@gmail.com>
2022-09-18 20:25:34 -0600

    added the function get_watched_avg_rating - wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,22 +10,15 @@ def create_movie(title, genre, rating):
     else:
         return None
 
-#user data is a dictionary with a key "watched" and value is list of dictionaries 
-#movie is a dictionary with title, genre,rating as keys (like create movie)
-#add movie to user_data & return user_data 
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
 
-#user_data is same as above exept key is "watchlist"
-#movie follows create_movie format
-#add movie to user_data & return user_data 
+
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
-#if title is in watchlist, move to watched
-#else return user_data 
 
 def watch_movie(user_data, title):
 
@@ -40,6 +33,15 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    rating_sum = 0
+    num_watched = len(user_data["watched"])
+    if num_watched == 0:
+        return 0
+    else:
+        for movie in user_data["watched"]:
+            rating_sum += movie["rating"]
+        return rating_sum/num_watched
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
