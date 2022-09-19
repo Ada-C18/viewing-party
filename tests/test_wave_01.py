9dd3331bff4711a757aad920e4f5799180a4868e
viviantomato <vivianyw924@gmail.com>
2022-09-19 00:02:57 -0700

    structure

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -100,7 +100,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] == GENRE_1
     assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -118,11 +118,12 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
+
+    #raise Exception("Test needs to be completed.")
+     
 
 @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
@@ -142,11 +143,10 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
+    assert updated_data["watched"][1]["title"] == HORROR_1
+
+    #raise Exception("Test needs to be completed.")
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
 
 @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -17,21 +17,66 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
+
+def watch_movie (user_data, title):
+    if title in user_data["watchlist"]:
+        #remove from list
+        user_data["watchlist"].remove(title)
+        add_to_watched(user_data, title)
+        return user_data
+    else:
+        return user_data
+        
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+'''
+def get_watched_avg_rating(user_data):
+    if len = 0
+    avr = 0.0
+    else:
+        sum rating / len
 
+def get_most_watched_genre(user_data):
+    count each genre sum and get max
+
+    if "watched" is empty, return None
+
+'''
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+'''
+#user watched but friends didnt
+def get_unique_watched(user_data):
+    intersecter??
 
+#friends watched but user didnt
+def get_friends_unique_watched(user_data):
+
+'''
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+'''
+#friends watched but user didnt, on host
+def get_available_recs(user_data):
 
+
+'''
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+'''
+# matching genre with the max 
+def get_new_rec_by_genre(user_data):
+
+# movie is in the user's "favorites"; 
+# None of the user's friends have watched it
+
+def get_rec_from_favorites(user_data):
+'''
