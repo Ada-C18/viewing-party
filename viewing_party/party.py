f273ef1856a4b4ca0a089e547383edab9d8a3af3
ivystrayed <ivystrayed@gmail.com>
2022-09-19 12:12:17 -0700

    Finished Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,29 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = {
+        "title" : title,
+        "genre" : genre,
+        "rating" : rating,
+    }
+    if all(movie.values()) == False:
+        return None
+    else:
+        return movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie):
+    for movie_dict in user_data["watchlist"]:
+        if movie_dict["title"] == movie:
+            user_data["watched"].append(user_data["watchlist"].pop())
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
