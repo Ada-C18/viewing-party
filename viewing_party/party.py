898b30147acd165b2aa67300b55574b702160a2d
June Valentino <vintydesign@gmail.com>
2022-09-18 21:05:35 -0700

    Finished wave 1 - cleared all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,10 +1,39 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+
+    if title == None or genre == None or rating == None:
+        return None
+
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+
+    return user_data
+
+
+
 # this is my minimal change code aaaa
 
-def watch_movie(janes_dict, title)
+# def watch_movie(janes_dict, title):
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
