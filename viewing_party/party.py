1d6e2a814930d043b3fb16afd25861cc91f74169
Marie Keefer <mkeefer17@gmail.com>
2022-09-20 18:19:40 -0400

    passing all wave 1 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -8,7 +8,27 @@ def create_movie(title, genre, rating):
         movie_dict.update({"rating": rating})
         return movie_dict
     else:
-        return None
+        return None   
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+    # for data in user_data:
+    #         data.append(movie)
+    # print(user_data)
+    # return user_data
+
+def add_to_watchlist(user_data, movie):
+    for watchlist in user_data:
+        watchlist.append(movie)
+
+def watch_movie(user_data, title):
+    if title in user_data["watchlist"]:
+        user_data["watchlist"].remove(title)
+        user_data["watched"].append(title)
+    else:
+        return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
