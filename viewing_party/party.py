2837383545a9ea89a1381d6bb2e8b34acfddde8d
Devin Taylor <devin.taylor122@gmail.com>
2022-09-19 21:54:43 -0700

    added function that updated watched and watchlist when movie is watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -20,6 +20,12 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if title in movie["title"]:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
