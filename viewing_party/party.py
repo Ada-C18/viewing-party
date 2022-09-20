a6617b036cba05664a48023345e377ee5c72a4ad
Laura Skomerza <lskomerza@gmail.com>
2022-09-20 13:14:44 -0700

    fixed wave 01 test 1 and started next function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -5,14 +5,26 @@ from operator import ge
 
 
 def create_movie(title, genre, rating):
-    if title or genre or rating:
-        dict = {}
-        dict["title"] = title
-        dict["genre"] = genre
-        dict["rating"] = rating
-        return dict
     
+    if title and genre and rating:
+        movie = {}
+        
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        return movie
+
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    prev = user_data["watched"] # creating a variable prev = the value of watched - we know key = watched
+    curr = prev.append(movie)
+    user_data["watched"] = curr # replacing the old empty value with the new value
+
+    return user_data
     
+
 # 
 # if parameters = True return a dict with 
 # {
@@ -21,10 +33,18 @@ def create_movie(title, genre, rating):
 # "rating": 5}
 # create empty dict
 
-    
+'''
+
+Create a function named create_movie. This function and all subsequent functions should be in party.py. create_movie should...
+take three parameters: title, genre, rating
+If those three attributes are truthy, then return a dictionary. This dictionary should...
+Have three key-value pairs, with specific keys
+The three keys should be "title", "genre", and "rating"
+The values of these key-value pairs should be appropriate values
+If title is falsy, genre is falsy, or rating is falsy, this function should return None
 
 
-
+'''
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
