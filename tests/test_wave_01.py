9b004b3661fe5e03aa6de1995b3c02ac10aa1644
Abby <abbychoi97@gmail.com>
2022-09-20 13:12:51 -0700

    added new assert expressions in test_moves_movie_from_watchlist_to_watched test

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -149,6 +149,8 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watched"]) == 2
     
     # raise Exception("Test needs to be completed.")
+    assert len(updated_data["watchlist"]) == 1
+    assert len(updated_data["watched"]) == 2
     assert updated_data["watchlist"] == [FANTASY_1]
     assert updated_data["watched"] == [FANTASY_2, movie_to_watch]
     # *******************************************************************************************
