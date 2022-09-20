d0b5c35c0ffc5a982f99eea94f739777ab783d3b
kindra greenawalt <kindra.kathleen@gmail.com>
2022-09-20 18:18:33 -0400

    Added asserts to wave1 2nd and 3rd to last tests. Completed wave 2 functions, including getting most watched genre funct to pass all wave 2 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -35,6 +35,45 @@ def watch_movie(user_data,title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    ratings = []
+    ratings_sum = 0
+
+    if(len(user_data["watched"])) is 0:
+        ratings_average = 0.0
+    else:
+        for i in range(len(user_data["watched"])):
+            ratings.append((user_data["watched"][i]["rating"]))
+            i+=1
+    
+        for rating in ratings:
+            ratings_sum += rating
+        
+        ratings_average = ratings_sum / len(ratings)
+
+    return ratings_average  
+
+def get_most_watched_genre(user_data):
+    genre_list = []
+    genre_dictionary = {}
+
+    if (len(user_data["watched"])) is 0:
+        popular_genre = None
+    else:
+        for i in range(len(user_data["watched"])):
+            genre_list.append((user_data["watched"][i]["genre"]))
+            i += 1
+    
+    for genre in genre_list:
+        count = genre_list.count(genre)
+        genre_dictionary[genre] = count
+        
+    popular_genre = max(genre_dictionary, key=genre_dictionary.get)
+    return(popular_genre)
+
+
+
+
 
 
 # -----------------------------------------
