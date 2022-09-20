c7cc52c07e059df66426714bd22ff7278234f5d3
Evelyn <hay.evelyn@gmail.com>
2022-09-20 14:51:17 -0700

    created add_to_watched func. Passed test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -5,12 +5,16 @@ def create_movie(title, genre, rating):
     
     if title == None or genre == None or rating == None:
         return None
-        
+
     movie_dict["title"] = title
     movie_dict["genre"] = genre
     movie_dict["rating"] = rating
     
     return movie_dict
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
