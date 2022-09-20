5446c551fc0cb3d2a288bba533a7ff6e5fde999c
Tia Willis <tiawillis1@icloud.com>
2022-09-19 23:20:24 -0400

    wave 2 complete, functions in wave 2: get_watched_avg_rating and get_most_watched_genr, all wave 2 tests passing. wave 3 started, first 2 tests passing.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,4 @@
 # ------------- WAVE 1 --------------------
-
 def create_movie(title, genre, rating):
     #pass
     if title and genre and rating:
@@ -21,23 +20,78 @@ def add_to_watchlist(user_data, movie):
 def watch_movie(user_data, title):
 
     for movie in user_data.get("watchlist"):
+
         if movie["title"] == title:
             user_data.get("watchlist").remove(movie)
             user_data.get("watched").append(movie)
-            return user_data
-    return user_data
 
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
 
+    if len(user_data.get("watched")) == 0:
+        return 0.0
+
+    rating_avergae = 0
+
+    for movie in user_data.get("watched"):
+        rating_avergae +=  movie["rating"]
+
+    return rating_avergae / len(user_data.get("watched"))
+
+def get_most_watched_genre(user_data):
+
+    if len(user_data.get("watched")) == 0:
+        return None
+
+    popular_genre = {}
+    for movie in user_data.get("watched"):
+        genre = movie["genre"] 
+
+        if genre not in popular_genre:
+            popular_genre[genre] = 1
+        else: 
+            popular_genre[genre] += 1
+
+    most_count = 0
+    most_popular_genre = ""
+
+    for g, value in popular_genre.items():
+
+        if value > most_count:
+            most_count = value 
+            most_popular_genre = g
+            
+    return most_popular_genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+
+    unique_watched = []
+    friend_movies = []
+
+    for friend in user_data.get("friends"):
+
+        for f_movie in friend.get("watched"):
+
+            if f_movie not in friend_movies:
+                friend_movies.append(f_movie)
+    
+    # trying list comprehension
+    unique_watched = [movie
+        for movie in user_data.get("watched")
+            if movie not in friend_movies]
+            
+    return unique_watched
         
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
