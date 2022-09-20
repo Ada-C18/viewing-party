3468d47225dfaeadd6425f5e32d4b347c53a12c5
viviantomato <vivianyw924@gmail.com>
2022-09-20 01:01:47 -0700

    most watched genre passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,10 @@
 # ------------- WAVE 1 --------------------
 
 # create movie as dictionary with title, genre, rating as key
+from curses import keyname
+from re import A
+
+
 def create_movie(title, genre, rating):   
     movie = {}
     if not title or not genre or not rating:
@@ -41,7 +45,7 @@ def get_watched_avg_rating(user_data):
     
     sum_rating = 0.0
     watched_length = len(user_data["watched"])
-    # access movie rating - user_data["watchlist"][0]["rating"] 
+    # access movie rating - user_data["watched"][0]["rating"] 
     if watched_length > 0:
         for movie in user_data["watched"]:
             sum_rating += movie["rating"]
@@ -49,10 +53,30 @@ def get_watched_avg_rating(user_data):
     else:
         return 0.0
 
-#def get_most_watched_genre(user_data):
-    #count each genre sum and get max
-
+# get most watched genre 
+def get_most_watched_genre(user_data):
+    # access movie genre - user_data["watched"][0]["genre"] 
+    
+    # create dict {}, genre : count, add to count if exsit, add key pair if non exsit; get max value from dict and return the key
     #if "watched" is empty, return None
+    genre_count = { }
+    watched_length = len(user_data["watched"])
+    if watched_length > 0:
+        for movie in user_data["watched"]:
+            # or append to a set of genre??
+            # if genre do not exsit in genre_count as dictionary key
+            if movie["genre"] not in genre_count.keys():
+                #add key value pair
+                genre_count[movie["genre"]] = 1
+            else:    
+                # key: add +1 to value
+                genre_count[movie["genre"]] += 1
+        
+        #get max from genre_count
+        return max(genre_count, key=genre_count.get)
+    else:
+        return None 
+    
 
 
 
