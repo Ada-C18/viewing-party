187b13148d479fac449398785fe7e3e00d1032e2
Alia Athman <aliaathmann@gmail.com>
2022-09-20 17:55:08 -0400

    Adds the function get_new_rec_by_genre, passing all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -114,4 +114,11 @@ def get_available_recs(user_data):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
+def get_new_rec_by_genre(user_data):
+    most_watched_genre = get_most_watched_genre(user_data)
+    recommended_movies = []
+    friends_unique_movies = get_friends_unique_watched(user_data)
+    for movie in friends_unique_movies:
+        if movie['genre'] == most_watched_genre:
+            recommended_movies.append(movie)
+    return recommended_movies
