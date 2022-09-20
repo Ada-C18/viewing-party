2523e84890889346668d689707efb1153b0aa02a
Misha <misha@Mishas-MacBook-Air.local>
2022-09-20 09:06:17 -0700

    adds functions for wave 4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -59,63 +59,61 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 def get_unique_watched(user_data): 
     friends_watched_list = []
-    user_watched_list = []
     unique_watched_list = []
     friends = user_data["friends"]
+    # maybe get rid of this variable later.. don't think you need it!! 
     user_watched = user_data["watched"]
     
     for friend in friends:
         for movie in friend["watched"]:
             friends_watched_list.append(movie["title"])
     
-
     for movie in user_watched:
         if movie["title"] not in friends_watched_list:  
             unique_watched_list.append(movie)
+    
     return unique_watched_list
 
-    
 
-    # print(f"debug info : {friends_watched_list = },\n {user_watched_list = }") 
-    # unique_watched = {}
-    # friends_watched_list = []
-    # user_watched_list = [] 
-    # unique_watched_list = []
-    # 
-    
-    # # for i in range(len(friend)):
-    #     # for i in range(len(friend[i]["watched"])):
-    #     #     friends_watched_list.append(["title"])
-    # for i in range(len(friend)):
-    #     for i in range(len(friend[i]['watched'])):
-    #         # movie_title = [i]['watched'][i]['title']
-    #         # print(movie_title)
-    #         friends_watched_list.append(friend['watched'][i]["title"])
-    
-    # # print(f"{friends_watched_list =}")
-    
-    # for i in range(len(user_data["watched"])):
-    #     user_watched_list.append(user_data["watched"][i]["title"])
-    # # print(f"{user_watched_list =}")
-    
-    # for movie in user_watched_list:
-    #     if movie not in friends_watched_list: 
-    #         # unique_watched.update({"movie": movie})
-    #         unique_watched_list.append(movie)
-    
-    # for movie in unique_watched_list:
-    #     unique_watched['movies'] = unique_watched_list
-        
-    
-    # print(f"debug info : {friends_watched_list = },\n {user_watched_list = },\n {unique_watched = }")        
-    # print(f"{unique_watched_list = }")    
-    # 
+def get_friends_unique_watched(user_data):
+    friends_unique_watched_list = []
+    user_list = []
+    friends_unique = []
+    friends = user_data["friends"]
+    user_watched = user_data["watched"]
 
+    for movie in user_watched:
+        user_list.append(movie["title"])
     
+    for friend in friends:
+        for movie in friend["watched"]:
+            if movie["title"] not in user_list: 
+                friends_unique_watched_list.append(movie)
+    
+    for i in range(len(friends_unique_watched_list)):
+        if friends_unique_watched_list[i] not in friends_unique_watched_list[i + 1:]:
+            friends_unique.append(friends_unique_watched_list[i])
+
+    return friends_unique
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-
+def get_available_recs(user_data):
+    friend_recs = []
+    final_recs = []
+    user_watched_list = [movies for movies in user_data["watched"]]
+    
+    for movie in user_data["friends"]:
+        for title in movie["watched"]:
+            if title not in user_watched_list:
+                if title not in friend_recs:
+                    friend_recs.append(title)
+    
+    for movie in friend_recs:
+        if movie["host"] in user_data["subscriptions"]:
+            final_recs.append(movie)
+    
+    return final_recs
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
