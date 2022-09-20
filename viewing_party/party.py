6471f821dc187f5fa5b825108015ae4117732615
Jessica Hebert <jessicahebert75@gmail.com>
2022-09-19 22:40:02 -0700

    wave 1 almost done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,13 +1,22 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    
     new_move = {"title": title,
                 "genre" : genre,
                 "rating": rating}
+    for key, value in new_move.items():
+        if value == None:
+            return None
+    else:
+        return new_move
 
-    return new_move
+def add_to_watched(user_data, movie):
+  user_data = {"watched":[movie]}
+  return user_data
 
+def add_to_watchlist(user_data,movie):
+  user_data = {"watchlist":[movie]}
+  return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
