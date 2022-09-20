d7ec23f1cf4909a3b9ec15ffb1af3768d657aed9
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2022-09-20 14:11:02 -0700

    "wave 3 test 1 complete"

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,6 @@
+import json
+from textwrap import indent
+
 # ------------- WAVE 1 --------------------
 
 # Instantiate movie as dictionary if all fields are populated
@@ -71,7 +74,36 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+#funciton to return unique movies from all friends watched lists
+
+def get_unique_watched(user_data):
+
+    #create set of titles that friends have watched
+    friend_watched_titles_list = []
+    for friends_watched_dict in user_data["friends"]:
+        for friends_watched_movies in friends_watched_dict["watched"]:
+            friend_watched_titles_list.append(friends_watched_movies['title'])
+    friend_watched_titles_set = set(friend_watched_titles_list)
+
+    #create set of titles the user has watched
+    user_watched_titles_list = []
+    for user_watched_movie in user_data["watched"]:
+       user_watched_titles_list.append(user_watched_movie['title'])
+    user_watched_titles_set = set(user_watched_titles_list)
+
+    #find the difference between the sets
+    movie_titles_unique_to_user = user_watched_titles_set.difference(friend_watched_titles_set)
+
+    #iterate through the user_watched list and append the movie dictionary to a list if the movie's title is in the unique to user set.
+    unique_to_user_watched = []
+    for movie in user_data["watched"]:
+        if movie["title"] in movie_titles_unique_to_user:
+            unique_to_user_watched.append(movie)
+
+    return unique_to_user_watched
+
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
