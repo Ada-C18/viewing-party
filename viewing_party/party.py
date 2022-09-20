defb1c381b383cac14eb991a16c43d77c9ae49b6
Geiselle Holt <geiselleholt@hotmail.com>
2022-09-19 21:44:50 -0600

    Wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,13 +1,13 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(movie_title, genre, rating):
-    new_movie = {
+    my_movie = {
         "title" : movie_title,
         "genre" : genre,
         "rating" : rating,
     }
 
-    return new_movie
+    return my_movie
 
 
 def add_to_watched(user_data, movie):
@@ -20,19 +20,19 @@ def add_to_watchlist(user_data, movie):
 
     return user_data
 
-def watch_movie(janes_data, movie):
-    janes_data["watched"].append(movie)
-    janes_data["watchlist"] = []
+def watch_movie(user_data, movie):
+    user_data["watched"].append(movie)
+    user_data["watchlist"] = []
 
-    return janes_data
+    return user_data
 
-def watched_movie(janes_data, movie_to_watch):
+def watched_movie(user_data, movie_to_watch):
 
-    if movie_to_watch in janes_data["watchlist"]:
-        janes_data["watched"].append(movie_to_watch)
-        janes_data["watchlist"].remove(movie_to_watch)   
+    if movie_to_watch in user_data["watchlist"]:
+        user_data["watched"].append(movie_to_watch)
+        user_data["watchlist"].remove(movie_to_watch)   
 
-    return janes_data
+    return user_data
 
 
         
@@ -41,6 +41,43 @@ def watched_movie(janes_data, movie_to_watch):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    watched_list = user_data["watched"]
+    all_ratings = []
+    for watch in watched_list:
+        all_ratings.append(watch["rating"])
+    rating_sum = sum(all_ratings)
+    if len(user_data["watched"]) == 0:
+        avg_rating = 0
+    else:
+        avg_rating = rating_sum/len(user_data["watched"])
+
+    return avg_rating
+
+
+def get_most_watched_genre(user_data):
+    genre_list = user_data["watched"]
+    all_genres = []
+    for genres in genre_list:
+        all_genres.append(genres["genre"])
+    
+    if len(all_genres) == 0:
+        return None
+    else:
+        counter = 0
+        num = all_genres[0]
+        
+        for i in all_genres:
+            frequency = all_genres.count(i)
+            if frequency > counter:
+                counter = frequency
+                num = i
+        return num
+    
+
+
+
+# def get_unique_watched(amandas_data):
 
 
 # -----------------------------------------
