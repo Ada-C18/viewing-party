a94e8913cf4f26a7bfa81096c34c4afe54fc7122
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-09-21 16:32:15 -0700

    added assert to test and commented out raise exception

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -38,7 +38,7 @@ def test_new_genre_rec_from_empty_watched():
     # Assert
     assert len(recommendations) == 0
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_new_genre_rec_from_empty_friends():
     # Arrange
     sonyas_data = {
@@ -52,8 +52,11 @@ def test_new_genre_rec_from_empty_friends():
             }
         ]
     }
-
-    raise Exception("Test needs to be completed.")
+    # Act
+    recommendations = get_new_rec_by_genre(sonyas_data)
+    # Assert
+    assert len(recommendations) == 0
+    # raise Exception("Test needs to be completed.")
     # *********************************************************************
     # ****** Complete the Act and Assert Portions of theis tests **********
     # *********************************************************************
@@ -72,7 +75,7 @@ def test_unique_rec_from_favorites():
     assert INTRIGUE_2b in recommendations
     assert sonyas_data == clean_wave_5_data()
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_unique_from_empty_favorites():
     # Arrange
     sonyas_data = {
