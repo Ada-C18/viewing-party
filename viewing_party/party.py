0bbef3a0c6596b78cf3bb713c71b7bdec5e2305f
Elsje Slothower <elsje.slothower@gmail.com>
2022-09-21 15:11:13 -0700

    wave 04 completedgit status

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -118,6 +118,14 @@ def get_friends_unique_watched(user_data):
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data):
+    recs = []
+    friends_movies = get_friends_unique_watched(user_data)
+    for movie in friends_movies:
+        if movie["host"] in user_data["subscriptions"]:
+            recs.append(movie)
+    return recs
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
