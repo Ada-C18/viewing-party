0e2cc50df002d0c7dcc8c4c4a2a04a2638d61a9a
Puja Mukherjee <pujam836@gmail.com>
2022-09-21 15:14:30 -0700

    wrote code for wave 2-func 2 and ran testcase 12 in wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -51,7 +51,7 @@ def watch_movie(user_data, title):
             user_data["watched"].append(temp_dict)
     return user_data
 
-#wave 2 part 1
+#wave 2 part 1-ran testcase 1 and 2 
 def get_watched_avg_rating(user_data):
     
     total_rating = 0
@@ -61,6 +61,39 @@ def get_watched_avg_rating(user_data):
     for i in range (0, len(temp_list_of_dict)):
         total_rating+=temp_list_of_dict[i]["rating"]
     return total_rating/len(temp_list_of_dict)
+
+#wave 2 part 2 
+def get_most_watched_genre(user_data):
+    #return the genre that is most frequently occuring in "watched" list
+    #if "watched" list is empty, return NOne 
+    dict_genre={}
+    max = 0
+    temp_list_of_moviedict = user_data["watched"] 
+        # [FANTASY_1, 
+        # FANTASY_2, 
+        # FANTASY_3, 
+        # ACTION_1, 
+        # INTRIGUE_1, 
+        # INTRIGUE_2]
+         
+    for i in range(0, len(temp_list_of_moviedict)):
+        genre_value = temp_list_of_moviedict[i]["genre"] #fantasy
+        if genre_value in dict_genre:
+            val = dict_genre[genre_value] #val=
+            val = val+1 #val=2
+            if val> max:
+                max = val
+                ans = genre_value #fantasy
+        else:
+            dict_genre[genre_value] = 1 #fantasy-1
+            if dict_genre[genre_value]> max:
+                ans = genre_value #ans= 
+    return ans
+    
+    
+
+
+
     
             
 
