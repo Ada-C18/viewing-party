954ec121c3835d37860ca93b72c931acfe68deea
Ariel <gabriellaw15@gmail.com>
2022-09-20 17:12:37 -0700

    Function get_watched_avg_rating defined and passes test test_calculates_watched_average_rating. In wave two.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,19 +18,26 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 def watch_movie(user_data, title):
-    # index = 0
+
     for movie in user_data["watchlist"]:
         if title in movie["title"]:
             user_data["watchlist"].remove(movie)
             user_data["watched"].append(movie)
-            # index += 1
+            
     return user_data
             
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
+def get_watched_avg_rating(user_data):
+    total_ratings = 0
+    number_of_movies = 0
+    for movie_dict in user_data["watched"]:
+        total_ratings += movie_dict["rating"]
+        number_of_movies += 1
+    average_rating = total_ratings/number_of_movies
+    return average_rating
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
