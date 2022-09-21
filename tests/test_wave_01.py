d00de2ebe31ee151b81b3a5f9d2b1f50af6d2c16
Liat <liathirsh@gmail.com>
2022-09-21 16:47:09 -0400

    -m completed wave 3 tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -140,7 +140,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    assert updated_data['watched'][0]['title'] == FANTASY_2['title']
+    assert updated_data['watched'][0] == FANTASY_2
     #raise Exception (Add assertions here to test that the correct movie was added to "watched") 
 
 #@pytest.mark.skip()
