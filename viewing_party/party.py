e96ef21b62d230f36a9fd415782726633d39ff90
Siyi Ji <monkeyqueenstacy@gmail.com>
2022-09-21 13:32:57 -0400

    wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,8 @@
 # ------------- WAVE 1 --------------------
 
+import math
+
+
 def create_movie(title, genre, rating):
     movie = {}
     if title and genre and rating:
@@ -30,24 +33,39 @@ def watch_movie(user_data, title):
             return user_data
     return user_data
 
-janes_data = {
-            "watchlist": [{
-                "title": "a",
-                "genre": "b",
-                "rating": "c"
-            }, {
-                "title": "a1",
-                "genre": "b1",
-                "rating": "c1"
-            }],
-            "watched": []
-        }
-for ele in janes_data["watchlist"]:
-    for x in ele:
-        print(x)
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+    sum_rating = 0.0
+    length = len(user_data["watched"])
+    if "watched" in user_data and length > 0:
+        for ele in user_data["watched"]:
+            sum_rating += ele["rating"]
+        avg_rating = sum_rating/length
+    else:
+        avg_rating = 0.0
+    return avg_rating
+
+
+def get_most_watched_genre(user_data):
+    counter = {}
+    res = None
+    if "watched" in user_data:
+        for ele in user_data["watched"]:
+            if not ele["genre"] in counter:
+                counter[ele["genre"]] = 1
+            else:
+                counter[ele["genre"]] += 1
+        max_count = 0
+        for k, v in counter.items():
+            if v > max_count:
+                res = k
+                # max_count = v
+    return res
+
 
 
 # -----------------------------------------
