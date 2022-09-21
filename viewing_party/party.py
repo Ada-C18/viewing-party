b176f432721a4b14a60483721381a3c144e6060f
galinaramaniuk <galina.sergarz@gmail.com>
2022-09-20 23:27:32 -0400

    Added code for WAVE 1. Tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,36 @@
+import copy
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title == None or genre == None or rating == None:
+        new_movie = None
+        return new_movie
+    
+    new_movie = {}
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+    return new_movie
+
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+def watch_movie(janes_data, title):
+    copied_data = copy.deepcopy(janes_data["watchlist"])
+    for movie in copied_data:
+        if movie["title"] == title:
+            janes_data["watched"].append(movie)
+            janes_data["watchlist"].remove(movie)
+    return janes_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
