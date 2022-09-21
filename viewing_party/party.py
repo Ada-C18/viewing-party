99223d7fef03f3449115c9dfb5b8850c8c6f8ced
kimiahasani <kn.improve@gmail.com>
2022-09-21 09:12:37 -0700

    wave 4 passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -97,15 +97,16 @@ def get_friends_unique_watched(user_data):
     return unique_friend_watched
 
 
-
-
-
-
-        
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-
+def get_available_recs(user_data):
+    recommended_movies=[]
+    unique_watched = get_friends_unique_watched(user_data)
+    for movie in unique_watched:
+        if movie["host"]in user_data["subscriptions"]:
+            recommended_movies.append(movie)
+    return recommended_movies
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
