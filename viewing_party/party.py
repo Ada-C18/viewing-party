b498e2af527ac91d3a0197fcd25b3304de0a1542
Kate <varindakate@gmail.com>
2022-09-21 09:43:35 -0700

    22 tests passed. WAVE05 in progress.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -107,4 +107,11 @@ def get_available_recs(user_data):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 #
-#
+def get_new_rec_by_genre(user_data):
+    recommend = []
+    most_watched_genre = get_most_watched_genre(user_data)
+    friends_watched = get_friends_unique_watched(user_data)
+    for movie in friends_watched:
+        if movie["genre"] == most_watched_genre:
+            recommend.append(movie)
+    return recommend
