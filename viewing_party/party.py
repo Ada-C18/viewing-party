778e64d1454a6b411230daa80e0817b3c3a4aaed
sabraha0311 <sabraha0311@gmail.com>
2022-09-20 18:09:37 -0700

    add_to_watched and add_to_watchlist passing tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,30 +1,42 @@
 # ------------- WAVE 1 --------------------
 
+# 1.1
 def create_movie(title, genre, rating):
+    if title and genre and rating:
+        movie_dict = {
+                "title": title,
+                "genre": genre,
+                "rating": rating
+            }
+    
+        return movie_dict
+    else:
+        return None
 
- # 1.1   
-    # movie = {}
-    # return movie
-
-
-    # if 3 attributes are truthy return dictionary
-
-    movie_dict = {
-            "title": title,
-            "genre": genre,
-            "rating": rating
-        }
-
-    return movie_dict
-
-    # else:
-    #     return None
+# 1.2
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
 
+# # 1.3
+def add_to_watchlist(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+    
 
+# # 1.4
+# def watch_movie(user_data, title):
+#     user_data: {"watchlist", "watched"},
+#     title: ""
 
-# def add_to_watched(user_data, movie):
-    # user_data will be a dictionary with key "watched" 
+# If the title is in a movie in the user's watchlist:
+# remove that movie from the watchlist
+# add that movie to watched
+# return the user_data
+# If the title is not a movie in the user's watchlist:
+# return the user_data
+
 
 
 # -----------------------------------------
