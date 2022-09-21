2364a05267b6e7011cb53d078d262088edefb369
arika <arikaagnew@gmail.com>
2022-09-21 11:06:48 -0400

    wave 2 and 3 complete all tests pass

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,8 @@
 # ------------- WAVE 1 --------------------
 
+from tests.test_constants import USER_DATA_2
+
+
 def create_movie(title, genre, rating):
     movie_dict = {}
     movie_dict["title"] = title
@@ -37,13 +40,60 @@ def watch_movie(user_data,title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    watched_value = []
+    avg = 0
+    
+    for key in user_data["watched"]:
+        avg += 1
+        watched_value.append(key["rating"])
+    
+    if avg == 0:
+        return 0.0
+    
+    average = sum(watched_value) / len (watched_value)
+    return average
+
+def get_most_watched_genre(user_data):
+    watched_genre = []
+    for key in user_data["watched"]:
+        watched_genre.append(key["genre"])
+    return max(set(watched_genre), key = watched_genre.count)
 
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    my_movies = []
+    same_movies = []
+
+    for movie in user_data["watched"]:
+        for friend in user_data["friends"]:
+            if movie in friend["watched"]:
+                same_movies.append(movie)
+            print(movie, same_movies)
+
+    for movies in user_data["watched"]:
+        if movies not in same_movies:
+            my_movies.append(movies)
+    return my_movies    
         
+def get_friends_unique_watched(user_data):
+    friends_unique_watched = []
+    friends_movie_list = []
+    user_movie_list = user_data["watched"]
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in friends_movie_list:
+                friends_movie_list.append(movie)
+
+    for movie in friends_movie_list:
+        if movie not in user_movie_list:
+            friends_unique_watched.append(movie)
+    return friends_unique_watched
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -51,4 +101,3 @@ def watch_movie(user_data,title):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
