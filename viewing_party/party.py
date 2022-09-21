d496e59699ccc24f2cd520eaf8262f58b38008ab
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2022-09-21 12:35:00 -0400

    Wave 3 was added

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,13 +1,20 @@
 # ------------- WAVE 1 --------------------
 
+from tkinter.tix import InputOnly
+
+
 def create_movie(title, genre, rating):
+    if not title or not genre or not rating:
+        return None
+        # validate input
     new_dict = {}
     new_dict["title"] = title
     new_dict["genre"] = genre
     new_dict["rating"] = rating
-    for key in new_dict:
-        if not new_dict[key]:
-            return None
+    # if not new_dict["key"]
+    # for key in new_dict:
+    #     if not new_dict[key]:
+    #         return None
     return new_dict
 
 
@@ -34,6 +41,9 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 
 def get_watched_avg_rating(user_data):
+    if not user_data["watched"]:
+        return 0.0
+
     rating_list=[]
     for movie in user_data["watched"]:
         if movie["rating"]> 0:
@@ -43,24 +53,23 @@ def get_watched_avg_rating(user_data):
     ave_rating=rating_sum/len(rating_list)
     
     return ave_rating
+    
 
-
-# def get_most_watched_genre(user_data):
-#     frequent_genre={}
-#     for movie in user_data["watched"]:
-#         # if genre in frequent_genre:
-#             frequent_genre+= 1
-#         else:
-#             frequent_genre+=1
-#         return frequent_genre
-#     else:
-#         return None
-
-
-        
-
-# def get_unique_watched(user_data):
-
+def get_most_watched_genre(user_data):
+    if not user_data["watched"]:
+        return None
+    frequent_genre_counter={}
+    for movie in user_data["watched"]:
+        genre = movie["genre"]
+        if genre in frequent_genre_counter:
+            frequent_genre_counter[genre]+=1
+        else:
+            frequent_genre_counter[genre]=1
+    max_genre = max(frequent_genre_counter.values())
+    for key in frequent_genre_counter:
+        if frequent_genre_counter[key]== max_genre:
+            return key
+    
 
 # -----------------------------------------
 
@@ -68,14 +77,93 @@ def get_watched_avg_rating(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 
+def get_unique_watched(user_data):
+    # user_data= watched
+    # input return user_data which is a dict
+    # output return list of dictionaries, (movies we've seen only
+
+    #=movies I saw
+    
+    # movies_i_saw=user_data["watched"]
+    # movies_they_saw=user_data["friends"]["watched"]
+    my_movies=[]
+    same_movies=[]
+    
+    for movie in user_data["watched"]:
+        for friend in user_data["friends"]:# friend is a dict with watched key referring to list of movies
+            if movie in friend["watched"]:
+                same_movies.append(movie)
+    for movies in user_data["watched"]:
+        if movies not in same_movies:
+            my_movies.append(movies)
+    return  my_movies
+
+def get_friends_unique_watched(user_data):
+    friend_movies=[]
+    # same_movies=[]
+    # for friend in user_data["friends"]:# friend is a dict with watched key referring to list of movies
+    #           for movie in user_data["watched"]:
+    #         if movie in friend["watched"]:
+    #             same_movies.append(movie)
+    #             print(f"{friend}")
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in user_data["watched"] and movie not in friend_movies:
+                friend_movies.append(movie)
+    return friend_movies
+
+
+    """
+    user_data= watched
+    input return user_data which is a dict
+    output return list of dictionaries, (movies we've seen only
+
+    user_data["watched"]#=movies I saw
+    user_data["friends"]["watched"]
+    movies_i_saw=["Jaws","Titanic","Avatar"]
+    movies_they_saw=["Jaws","Inception"]
+    final_answer=[list of movies I've seen and friends haven't]
+    delete_movies=[this is where shared movies go]
+    for elem in movies_i_saw:
+        if elem in movies_they_saw:
+            delete_movies.append(elem)
+    for movies in movies_i_saw:
+        if movies not in delete_movies:
+            final_answer.append(movies)
+    return final_answer
+
+    """
 
 # -----------------------------------------
-
+#input: dict user_data
+#outpus: list of dictionaries
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
+
+def get_available_recs(user_data):
+    rec_movie_list=[]
+    subscriptions_list= user_data["subscriptions"]
+    host_str_list= friend["watched"]["movie"]
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in user_data["watched"] and movie in rec_movie_list and friend["watched"]["movie"]:
+                rec_movie_list.append()
 # -----------------------------------------
 
 # -----------------------------------------
+#input=
+#output=
 # ------------- WAVE 5 --------------------
+# def get_new_rec_by_genre(user_data):
+#     rec_movies=[]
+#     #genre=
+#     # get most frequent movie["genre"]
+
+#     if movie not in user_data["watched"] and movie in rec_movies and genre the same as user frequent
+#     return rec_movies
+
+# def get_rec_from_favorites(user_data):
+
 # -----------------------------------------
