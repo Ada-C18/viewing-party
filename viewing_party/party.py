b218c1286ce03ae57e1860493abe26dba6af9db2
Yael <yaelechols@gmail.com>
2022-09-21 08:00:58 -0700

    Call add_to_watched in watch_movie

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -21,7 +21,7 @@ def add_to_watchlist(user_data, movie):
 def watch_movie(user_data, title):
     for movie in user_data["watchlist"]:
         if title == movie["title"]:
-            user_data["watched"].append(movie)
+            add_to_watched(user_data, movie)
             user_data["watchlist"].remove(movie)
     return user_data
 
