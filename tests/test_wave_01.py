02a3bbb7ccb634cc815eeb0e32830aedc2a7dcf0
Liat <liathirsh@gmail.com>
2022-09-21 16:41:10 -0400

    -m completed tests in wave 1

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -118,12 +118,9 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    #raise Exception("Test needs to be completed.")
-    
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data['watched'][0]['title'] == MOVIE_TITLE_1
+
+    # COMPLETED --  Add assertions here to test that the correct movie was added to "watched" **********
 
 #@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
@@ -143,11 +140,8 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    
-    #raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data['watched'][0]['title'] == FANTASY_2['title']
+    #raise Exception (Add assertions here to test that the correct movie was added to "watched") 
 
 #@pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
