f69701e7b12aa98e1f90b2d1282875b7d22a5b54
Amanda <amanda.scott17@gmail.com>
2022-09-21 08:17:00 -0700

    wave 2 complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -45,7 +45,40 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+  rating_list = []
+  average = 0.0
+  for movie_list in user_data["watched"]:
+    if len(movie_list) != 0:
+      rating_list.append(movie_list["rating"])
+      rating_sum = sum(rating_list)
+      count_of_ratings = len(rating_list)
+      average = rating_sum / count_of_ratings
+  
+    else:
+      return average
+  return average
 
+
+def get_most_watched_genre(user_data):
+  genres_list = []
+  genres_dict = {}
+  count = 0
+  for movie_list in user_data["watched"]:
+    if len(movie_list) != 0:
+      genre_title = movie_list["genre"]   
+      if genre_title in genres_list:
+        count += 1
+        genres_dict[count] = count
+      else:
+        genres_list.append(genre_title)
+        genres_dict[genre_title] = 1
+      
+        return genres_list[0]
+        
+    return None
+      
+  
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
