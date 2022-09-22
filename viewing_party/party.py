1309efb3413f109770a12b47d1a606545642bdf5
Milena Souto <yamadasouto@gmail.com>
2022-09-22 09:31:08 -0700

    Remove comments from party file

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,6 @@
 # import json
 # print(json.dumps(user_data,indent=2))
 # ------------- WAVE 1 --------------------
-# 1. Create a function named  `create_movie`. This function and all subsequent functions should be in `party.py`. 
 
 def create_movie(title, genre, rating):
     if title and genre and rating:
@@ -12,22 +11,17 @@ def create_movie(title, genre, rating):
         return movie_dict
     return None
 
-# 2. Create a function named `add_to_watched`.
-# test: test_adds_movie_to_user_watched
+
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
 
-# 3. Create a function named add_to_watchlist.def 
-# test: test_adds_movie_to_user_watchlist()
+
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
-# 4. Create a function named watch_movie.
-# TODO: move dict from watchlist to watched list if movie in user's watchlist
-# OUTPUT: user_data (modified user_data if title in watchlist)
-# test_moves_movie_from_watchlist_to_watched
+
 def watch_movie(user_data, title):
     watchlist = user_data["watchlist"]
     for movie in watchlist:
@@ -44,9 +38,6 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # Note: For Waves 2, 3, 4, and 5, your implementation of each of the functions should not modify user_data.?
 
-# 1. Calculate the average rating of all movies in the watched list
-# The average rating of an empty watched list is 0.0
-# return the average rating
 
 def get_watched_avg_rating(user_data):
     rating_total = 0
@@ -60,10 +51,6 @@ def get_watched_avg_rating(user_data):
         rating_total += movie["rating"]
     return rating_total / watched_lst_length
 
-
-# 2. Determine which genre is most frequently occurring in the watched list
-# return the genre that is the most frequently watched
-# If the value of "watched" is an empty list, get_most_watched_genre should return None.
 
 def get_most_watched_genre(user_data):
     watched_lst = user_data["watched"]
@@ -135,17 +122,6 @@ def one_friend_has_seen(movie, friendList):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-# 1. Create a function named get_available_recs. 
-# Determine a list of recommended movies. A movie should be added to this list if and only if:
-# The user has not watched it
-# At least one of the user's friends has watched
-# The "host" of the movie is a service that is in the user's "subscriptions"
-
-# REQ 1: if movie not in user's watched list
-# REQ 2: if movie in at least 1 friend's watched list
-# REQ 3: if "host" str is in user's subscriptions lst
-
-# Return the list of recommended movies
 
 def get_available_recs(user_data):
     user_watched_lst = user_data["watched"]
