8bb2d7d25f9b3ecdf62bb873fe6957308f6aff3b
masinakh <nakhjirimasi@gmail.com>
2022-09-21 22:31:19 -0700

    wave 4 is done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -62,10 +62,11 @@ def get_most_watched_genre(user_data):
             genre_freq[movie["genre"]] += 1
         else:
             genre_freq[movie["genre"]] = 1
-    return max(genre_freq, key = lambda genre : genre_freq[genre])
+    
 
-
-
+    for genre, value in genre_freq.items():
+        if value == max(genre_freq.values()):
+            return genre 
 
 
 # -----------------------------------------
@@ -88,14 +89,19 @@ def get_unique_watched(user_data):
 
 
 def get_friends_unique_watched(user_data): 
-    # user_unique_watched = set(get_unique_watched(user_data))
-    freiend_movies_watched = set()
+    
+
+    freinds_unique_watched = []
+
     for friend in user_data['friends']:
-        freiend_movies_watched.update(friend["watched"])
-
-    return list(freiend_movies_watched - set(user_data['watched']))
-
-
+        
+        for title in friend['watched']:
+            # print(title)
+            if title not in freinds_unique_watched and title not in user_data['watched']: 
+                # print(title)
+                freinds_unique_watched.append(title)
+    return freinds_unique_watched          
+    
 
 
         
@@ -104,6 +110,27 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 
 # -----------------------------------------
+
+def get_available_recs(user_data):
+    
+   
+    friends_unique_movies = get_friends_unique_watched(user_data)
+    
+    rec_movies= []
+    for movie in friends_unique_movies:
+        if movie["host"] in user_data['subscriptions']:
+            rec_movies.append(movie)
+
+    return rec_movies
+
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+def get_new_rec_by_genre(user_data):
+    friends_unique_movies = get_friends_unique_watched(user_data)
+
+    most_watched_genre = get_most_watched_genre(user_data)
+
+
+    for movie in user_data["watched"]:
+        print(movie)
