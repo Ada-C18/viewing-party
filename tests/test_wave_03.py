cf396f1c43fbe15f182941199f48725c69da7790
Tazmeen <t.akhtar95@gmail.com>
2022-09-22 18:18:01 -0400

    changed test wave 3 test

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -61,7 +61,6 @@ def test_friends_unique_movies_not_duplicated():
     # ****** Add assertions here to test that the correct movies are in friends_unique_movies **********
     # **************************************************************************************************
 
-    assert friends_unique_movies not in amandas_data["watched"]
     assert INTRIGUE_3 in friends_unique_movies
     assert HORROR_1 in friends_unique_movies
     assert FANTASY_4 in friends_unique_movies
