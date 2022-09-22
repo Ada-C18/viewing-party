e4b3296cde95f8cc2a164a687025479c541d3ceb
Audrey Andoy <audreylandoy@gmail.com>
2022-09-22 08:35:21 -0400

    Revert "added create_movie logic"
    This reverts commit 9434c7039803cd637e827ca01514a282fe2c66de.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,14 +1,7 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    if not title or not genre or not rating:
-        return None
-
-    return {
-        "title": title,
-        "genre": genre,
-        "rating": rating
-    }
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
