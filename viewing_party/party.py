b1fe74de62bbf416ecff53a929e9ad07ce5cbe67
Yael <yaelechols@gmail.com>
2022-09-21 20:31:28 -0700

    Implement get_available_recs

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -105,13 +105,19 @@ def get_friends_unique_watched(user_data):
             unique_watched.append(friends_watched_dict[movie_title])
 
     return unique_watched
-
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 def get_available_recs(user_data):
-    pass
+    recommended_movies = []
+    friends_unique_watched = get_friends_unique_watched(user_data)
+
+    for movie in friends_unique_watched:
+        if movie["host"] in user_data["subscriptions"]:
+            recommended_movies.append(movie)
+
+    return recommended_movies
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
