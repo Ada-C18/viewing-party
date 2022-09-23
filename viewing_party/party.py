81d911fa57c71afdbc66b815e5542099c665e206
Ariam Yonas <ariam.yonas@hotmail.de>
2022-09-22 21:41:46 -0700

    added wave 4 part one function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -77,6 +77,7 @@ def get_unique_watched(user_data):
             for friend_movie in friend['watched']:
                 if movie['title'] == friend_movie["title"]:
                     watched = True 
+                    break
             if not watched:
                 result.append(movie)
     return result 
@@ -91,9 +92,9 @@ def get_friends_unique_watched(user_data):
                 if movie['title']== friend_movie['title']:
                     watched = True 
                     break
-                if not watched and friend_movie['title'] not in exists:
-                    results.append(friend_movie)
-                    exists.add(friend_movie['title'])
+            if not watched and friend_movie['title'] not in exists:
+                results.append(friend_movie)
+                exists.add(friend_movie['title'])
     return results
 
 
@@ -102,6 +103,22 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
+def get_available_recs(user_data):
+    result = []
+    exists = set ()
+    for friend in user_data['friends']:
+        for friend_movie in friend['watched']:
+            watched = False 
+            for movie in user_data['watched']:
+                if movie['title']== friend_movie['title']:
+                    watched = True
+                    break 
+            if not watched and friend_movie['title'] not in exists and friend_movie['host'] in user_data['subscriptions']:
+                result.append(friend_movie)
+                exists.add(friend_movie['title'])
+    return result 
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
