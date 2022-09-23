db28e0cdf0f31cbeed06b84419977c9e69515992
galinaramaniuk <galina.sergarz@gmail.com>
2022-09-23 00:44:17 -0400

    Code refactoring

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,14 +2,15 @@ import copy
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    if title == None or genre == None or rating == None:
+    if not title or not genre or not rating:
         new_movie = None
         return new_movie
     
     new_movie = {}
-    new_movie["title"] = title
-    new_movie["genre"] = genre
-    new_movie["rating"] = rating
+    if title and genre and rating:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
     return new_movie
 
 
@@ -23,39 +24,47 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 
-def watch_movie(janes_data, title):
-    copied_data = copy.deepcopy(janes_data["watchlist"])
+def watch_movie(user_data, title):
+    copied_data = copy.deepcopy(user_data["watchlist"])
+
     for movie in copied_data:
         if movie["title"] == title:
-            janes_data["watched"].append(movie)
-            janes_data["watchlist"].remove(movie)
-    return janes_data
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+
+    return user_data
 
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
-def get_watched_avg_rating(janes_data):
-    average_rating = 0
-    if janes_data["watched"]:
-        for movie in janes_data["watched"]:
+def get_watched_avg_rating(user_data):
+    average_rating = 0.0
+
+    if user_data["watched"]:
+        for movie in user_data["watched"]:
             average_rating += movie["rating"]
     else:
         return average_rating
-    return average_rating / len(janes_data["watched"])
+
+    return average_rating / len(user_data["watched"])
 
 
-def get_most_watched_genre(janes_data):
+def get_most_watched_genre(user_data):
     genre_frequency = {}
-    if not janes_data["watched"]:
+
+    if not user_data["watched"]:
         return None
-    for movie in janes_data["watched"]:
+
+    for movie in user_data["watched"]:
         if movie["genre"] not in genre_frequency:
             genre_frequency[movie["genre"]] = 1
         else:
             genre_frequency[movie["genre"]] += 1
+
     max_value = max(genre_frequency.values())
+
     for genre, value in genre_frequency.items():
         if value == max_value:
             return genre
@@ -69,13 +78,15 @@ def get_unique_watched(user_data):
     friends_movies = []
     user_unique_watched = []
 
-    for friend in user_data.get("friends"):
+    for friend in user_data["friends"]:
         for fr_movie in friend["watched"]:
             if fr_movie not in friends_movies:
                 friends_movies.append(fr_movie)
-    for user_movie in user_data.get("watched"):
+
+    for user_movie in user_data["watched"]:
         if user_movie not in friends_movies:
             user_unique_watched.append(user_movie)
+
     return user_unique_watched
 
 
@@ -83,14 +94,17 @@ def get_friends_unique_watched(user_data):
     friends_movies = []
     fr_unique_movies = []
 
-    for friends in user_data.get("friends"):
-        for fr_movie in friends.get("watched"):
+    for friends in user_data["friends"]:
+        for fr_movie in friends["watched"]:
             if fr_movie not in friends_movies:
                 friends_movies.append(fr_movie)
-    user_movies = user_data.get("watched")
+
+    user_movies = user_data["watched"]
+
     for fr_movie in friends_movies:
         if fr_movie not in user_movies:
             fr_unique_movies.append(fr_movie)
+
     return fr_unique_movies
 
 
@@ -101,9 +115,11 @@ def get_friends_unique_watched(user_data):
 def get_available_recs(user_data):
     recommended_movies = []
     unique_movies = get_friends_unique_watched(user_data)
+
     for movie in unique_movies:
         if movie["host"] in user_data["subscriptions"]:
             recommended_movies.append(movie)
+
     return recommended_movies
 
 
