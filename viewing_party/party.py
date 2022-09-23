c5e2e00fbbe09c02601776f8cd1cc4b37b8296b4
bukunmig <bgesinde44@icloud.com>
2022-09-23 08:25:27 -0700

    attempted viewing party 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,25 +1,39 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    movie_dict = {'title': 'Paw Patrol', 'genre' : 'family', 'rating':2.0}
-    if 'title' and 'genre' and 'rating' in movie_dict:
+    movie_dict = {'title': title, 'genre' : genre, 'rating': rating}
+    if title and genre and rating:
         return movie_dict
     else:
         return None
     
 def add_to_watched(user_data,movie):
-    user_data.update({'watched': })
+    user_data['watched'].append(movie)
     return user_data
 
 
 def add_to_watchlist(user_data,movie):
-    pass
-
-
+    user_data['watchlist'].append(movie)
+    return user_data
 
 
 def watch_movie(user_data,title): 
-    print(user_data)
+    movie_exist = False
+    i = 0
+
+    for movie in user_data['watchlist']:
+        if movie['title']==title:
+            
+            user_data['watched'].append(user_data['watchlist'].pop(i))
+            return user_data
+            movie_exist = True
+    i+=1
+           
+            
+    
+  
+    return user_data
+
 
 
 # -----------------------------------------
@@ -27,8 +41,33 @@ def watch_movie(user_data,title):
 # -----------------------------------------
 
 def get_watched_avg_rating(user_data):
-    average_rating = sum('watched')/len('watched')
+    rating = 0.0
+    for movie in user_data['watched']:
+        rating +=movie['rating']
+
+    average_rating = rating/len(user_data['watched'])
     return average_rating
+
+def get_most_watched_genre(user_data):
+    counter = 0
+    genre_list = []
+    genre_dict = {}
+    for movie in user_data['watched']:
+        genre_list.append(movie.get('genre'))
+    i = 0
+    genre_loc = 0
+    for genre in genre_list:
+        genre_count = genre_list.count(genre)
+        if genre_count > counter:
+            counter = genre_count 
+            genre_loc = i
+        genre_dict.update({'genre': genre_count })
+    a = sorted(genre_dict.items(), key=lambda x: x[1])
+    for genre in a:
+        genre_frequent =genre
+
+    return a[-1][0]
+    
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
