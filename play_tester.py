1dee11c2047667737c5f25a88e06d6e474332d6b
Masha <iiureva.mariia@gmail.com>
2022-09-22 17:21:46 -0700

    Committing all the viewing party package, all 5 waves

--- play_tester.py
+++ play_tester.py
@@ -9,21 +9,36 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
-print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# print("\n-----Wave 01 test data-----")
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
+
+janes_data = {
+    "watchlist": [FANTASY_1],
+    "watched": [FANTASY_2, HORROR_1, ACTION_1, FANTASY_1, FANTASY_2, HORROR_1, ACTION_1, FANTASY_1]
+    }
+
+    # Act
+#updated_data = get_most_watched_genre(janes_data)
+
+#print(updated_data)
+#print(updated_data)
 
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
 
-#print("\n-----Wave 03 user_data-----")
-#pp.pprint(clean_wave_3_data())
+# print("\n-----Wave 03 user_data-----")
+# #pp.pprint(clean_wave_3_data())
 
+# wave_3_user_data = clean_wave_3_data()
+#print(wave_3_user_data)
+# new_data = get_friends_unique_watched(wave_3_user_data)
+# print(new_data)
 # Wave 04 user data
-#print("\n-----Wave 04 user_data-----")
-#pp.pprint(clean_wave_4_data())
-
-# Wave 05 user data
-#print("\n-----Wave 05 user_data-----")
-#pp.pprint(clean_wave_5_data())
+# print("\n-----Wave 04 user_data-----")
+# pp.pprint(clean_wave_4_data())
+# get_available_recs(clean_wave_4_data())
+# # Wave 05 user data
+print("\n-----Wave 05 user_data-----")
+pp.pprint(clean_wave_5_data())
