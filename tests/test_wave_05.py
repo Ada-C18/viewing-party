9d63545b58091db8171e80eb5ca5476284302635
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2022-09-22 22:34:47 -0700

    project finished

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -58,7 +58,7 @@ def test_new_genre_rec_from_empty_friends():
     # Assert
     assert len(recommendations) == 0
 
-    #raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *********************************************************************
     # ****** Complete the Act and Assert Portions of theis tests **********
     # *********************************************************************
