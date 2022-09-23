07f70fee1ea0c19fa96d494e81dfdcf59a346525
Carina <checarina@gmail.com>
2022-09-22 17:45:14 -0700

    Passes Wave 4 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -96,7 +96,13 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-
+def get_available_recs(user_data):
+    recs = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in user_data["watched"] and movie["host"] in user_data["subscriptions"]:
+                recs.append(movie)
+    return recs
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
