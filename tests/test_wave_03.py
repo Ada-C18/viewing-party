9ad9c7712e6dadb1588d1f1e73986d2c5e783319
hang tran <hangmtran101@gmail.com>
2022-09-22 22:10:41 -0700

    all waves finished

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -2,21 +2,26 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_my_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
+    print(amandas_data)
 
     # Act
     amandas_unique_movies = get_unique_watched(amandas_data)
+    
 
     # Assert
     assert len(amandas_unique_movies) == 2
     assert FANTASY_2 in amandas_unique_movies
     assert INTRIGUE_2 in amandas_unique_movies
     assert amandas_data == clean_wave_3_data()
-
-@pytest.mark.skip()
+    
+    
+    
+    
+# @pytest.mark.skip()
 def test_my_not_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -28,22 +33,24 @@ def test_my_not_unique_movies():
     # Assert
     assert len(amandas_unique_movies) == 0
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_friends_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
 
     # Act
     friends_unique_movies = get_friends_unique_watched(amandas_data)
-
+    
     # Assert
     assert len(friends_unique_movies) == 3
     assert INTRIGUE_3 in friends_unique_movies
     assert HORROR_1 in friends_unique_movies
     assert FANTASY_4 in friends_unique_movies
+    # print(friends_unique_movies)
     assert amandas_data == clean_wave_3_data()
+    
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_friends_unique_movies_not_duplicated():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -51,16 +58,17 @@ def test_friends_unique_movies_not_duplicated():
 
     # Act
     friends_unique_movies = get_friends_unique_watched(amandas_data)
-
+    print(amandas_data)
     # Assert
     assert len(friends_unique_movies) == 3
 
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *************************************************************************************************
     # ****** Add assertions here to test that the correct movies are in friends_unique_movies **********
     # **************************************************************************************************
-
-@pytest.mark.skip()
+    assert INTRIGUE_3 in friends_unique_movies
+    
+# @pytest.mark.skip()
 def test_friends_not_unique_movies():
     # Arrange
     amandas_data = {
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,5 @@
 # ------------- WAVE 1 --------------------
-import json
+
 
 def create_movie(title, genre, rating):
 
@@ -12,7 +12,7 @@ def create_movie(title, genre, rating):
     #returns none if title or movie or rating is falsy
     if title == None or genre == None or rating == None:
         return None 
-    #returns none if title or movie or rating is falsy
+    
     else: 
         return movie
 
@@ -22,21 +22,18 @@ def create_movie(title, genre, rating):
 
 
 def add_to_watched(user_data, movie):
-    #user data is dictionary w/ key "watched", value = [{}]
-    
+    #user data is dictionary w/ key "watched"
     
     user_data = {
         "watched": [movie]
     }
 
-    ## create_movie(title, genre, rating) #call create_movie function 
-
-#    last step to return user_data
+    
     return user_data
 
 def add_to_watchlist(user_data, movie):
     #user_data {"watchlist": list of dictionaries representing the movies the user wants to watch}
-    #if watchlist = [] means no movies in watchlist
+ 
     user_data = {"watchlist":[movie]}
 
     return user_data
@@ -46,7 +43,6 @@ def add_to_watchlist(user_data, movie):
 
 def watch_movie(user_data, title):
                
-    
    
     for movie_title in user_data["watchlist"]:
         if title == movie_title["title"]:
@@ -64,84 +60,152 @@ def get_watched_avg_rating(user_data):
 #     #find average rating by accessing the "ratings" key from "watched" list from user_data dictionary
 #user_data = {'watched': [{'genre': 'Fantasy', 'rating': 4.8, 'title': 'The Lord of the Functions: The Fellowship of the Function'}... 'rating': 2.0, 'title': 'Recursion'}, {'genre': 'Intrigue', 'rating': 4.5, 'title': 'Instructor Student TA Manager'}]}
 
-    rating_list = [] #is this actually testing the function in pytest?
-    
-    if rating_list is None: #if empty list
-        return 0.0
-    
-    for watch_list in user_data["watched"]: 
-        for movie_rating in watch_list:
-            # print(movie_rating)
-            rating_list.append(movie_rating["rating"])
-            
-            
-    try: #not sure if I need to add this
-        average = sum(rating_list)/len(rating_list)
-    except ZeroDivisionError:
-        average = 0
-    
-    return format(average,".6f") #how do I get the format so that it is 3.58333
-
-
-
-    
-
-
-
-
-
-    
-# def get_most_watched_genre(user_data):
-#     #access the genre by user_data["watched"]
-#     # to get the most frequently occuring in watched list, use a counter and for loop
-
-#     # popular_genre = 0
-    
-#     genre_list = []
-
-#     if len(user_data["watched"]) == 0:
-#         return None
-
-#     else:
-#         for watch_list  in user_data["watched"]:
-#             for movie_dict in watch_list:
-#                 genre_list.append(movie_dict["genre"])
-
-       
+    rating_list = []
    
- 
 
-#      #not sure if we can use this but this should give the element with highest frequency , will use counter if not    
-#     return max(set(genre_list), key =genre_list.count)
+    for movie in user_data["watched"]:
+    
+        rating_list.append(movie["rating"])
+    if len(user_data["watched"]) == 0:
+        return 0.0
+        
+    else:
+        return sum(rating_list)/len(rating_list)
+    
+   
+
+    
+def get_most_watched_genre(user_data):
+    #access the genre by user_data["watched"]
+   
+
+    genre_list = []
+
+    if len(user_data["watched"]) == 0:
+        return None
+
+    else:
+        for watch_list  in user_data["watched"]:
+            genre_list.append(watch_list["genre"])
+
+    return max(set(genre_list), key =genre_list.count)
 
 
- 
+
+# # -----------------------------------------
+# # ------------- WAVE 3 --------------------
+# # -----------------------------------------
+def get_unique_watched(user_data):
+    
+# initiated two emtpy list: user and friends
+#we have an outer dictionary, one with "watched" key and one with "friends " key
+# {'watched': [{'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, {'title': 'The Lord of the Functions: The Two Parameters', 'genre': 'Fantasy', 'rating': 4.0}, {'title': 'The Lord of the Functions: The Return of the Value', 'genre': 'Fantasy', 'rating': 4.0}, {'title': 'The JavaScript and the React', 'genre': 'Action', 'rating': 2.2}, {'title': 'Recursion', 'genre': 'Intrigue', 'rating': 2.0}, {'title': 'Instructor Student TA Manager', 'genre': 'Intrigue', 'rating': 4.5}],
+# 'friends': [{'watched': [{'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, {'title': 'The Lord of the Functions: The Return of the Value', 'genre': 'Fantasy', 'rating': 4.0}, {'title': 'The Programmer: An Unexpected Stack Trace', 'genre': 'Fantasy', 'rating': 4.0}, {'title': 'It Came from the Stack Trace', 'genre': 'Horror', 'rating': 3.5}]}, {'watched': [{'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, {'title': 'The JavaScript and the React', 'genre': 'Action', 'rating': 2.2}, {'title': 'Recursion', 'genre': 'Intrigue', 'rating': 2.0}, {'title': 'Zero Dark Python', 'genre': 'Intrigue', 'rating': 3.0}]}]
+#They want to see the what the user saw that none of their friends saw, and return list of movies [{title:,genre:},]
+# loop through user_data and use if statement to see if user_data["watched"] is in user_data["friends"] 
+
+#The Lord of the Functions: The Fellowship of the Function'The Lord of the Functions: The Two Parameters',The Lord of the Functions: The Return of the Value,'The JavaScript and the React''Recursion'Instructor Student TA Manager'
+# 'The Lord of the Functions: The Fellowship of the Function''The Lord of the Functions: The Return of the Value''The Programmer: An Unexpected Stack Trace','It Came from the Stack Trace', 'The Lord of the Functions: The Fellowship of the Function',The JavaScript and the React','Recursion','Zero Dark Python'
+   
+    
+    friends_watched_titles = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["title"] not in friends_watched_titles:
+                friends_watched_titles.append(movie["title"])
+                
+    
+        
+    unique_movies = []
+    for user_watched_movie in user_data["watched"]:
+        if user_watched_movie["title"] not in friends_watched_titles:
+            unique_movies.append(user_watched_movie)
+    return unique_movies
+    
+    
+def get_friends_unique_watched(user_data):
+#get only one of the friend's movie watched list, compare it to the users to see what the friend has saw the user hasn't
 
 
+    user_list =[]
+    friend_unique_movies = []
+    friend_unique_no_duplicate = []
 
+    for i in user_data["watched"]:
+        user_list.append(i)
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in user_list:
+                friend_unique_movies.append(movie)
+    #removing duplicates
+    friend_unique_no_duplicate = [i for n, i in enumerate(friend_unique_movies)
+            if i not in friend_unique_movies[n + 1:]]
+    return friend_unique_no_duplicate
 
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
-# def get_unique_watched(user_data):
-#     #initiated two emtpy list: user and friends
-#     #loop through user_data
-
-#     #turn user/ friends list to set and find difference result = set_a - set_b
-
-#     user_list = []
-#     friends_list = []
-
-
-
+    
 
 
         
-# -----------------------------------------
-# ------------- WAVE 4 --------------------
-# -----------------------------------------
+# # -----------------------------------------
+# # ------------- WAVE 4 --------------------
+# # -----------------------------------------
+def get_available_recs(user_data):
+# # # {'watched': [{'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8, 'host': 'netflix'}
+# # # 'subscriptions': ['netflix', 'hulu']}
+# # #return a list with what the user has not wached that would be in the friends list, that the user has a subscription for
 
-# -----------------------------------------
-# ------------- WAVE 5 --------------------
-# -----------------------------------------
 
+    recommended_movies = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["host"] in user_data["subscriptions"]:
+                if movie not in user_data["watched"]:
+                    recommended_movies.append(movie)
+    return recommended_movies
+
+
+   
+
+
+
+
+
+# # # -----------------------------------------
+# # # ------------- WAVE 5 --------------------
+# # # -----------------------------------------
+def get_new_rec_by_genre(user_data):
+# we have to make a list of recommended movies from the friend's unique watched list IF it is in the most frequently watched genre
+
+    recommended_movies = []
+    most_watched_genre = get_most_watched_genre(user_data)
+    
+    
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["genre"] == most_watched_genre:
+                if movie not in user_data["watched"]:
+                    recommended_movies.append(movie)
+    return recommended_movies
+
+
+               
+
+
+
+def get_rec_from_favorites(user_data):
+# make a list of recommendations from user's favorite 
+
+    recommendations =[]
+    user_watched_movies = get_unique_watched(user_data)
+    for movie in user_data["favorites"]:
+        if movie in user_watched_movies:
+            recommendations.append(movie)
+    return recommendations
+    
+
+
+
+
+
+
+    
