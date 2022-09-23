3211549ebb5a6e3c4b0a02d4443ba5fc47b7e0ee
Maria Silva <hello@mariasilva.dev>
2022-09-23 11:42:52 -0500

    created add_to_watched and add_to_watchlist functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,25 @@
 # ------------- WAVE 1 --------------------
-
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if len(title) > 0:
+        new_movie["title"] = title
+        if len(genre) > 0:
+            new_movie["genre"] = genre
+            if rating:
+                new_movie["rating"] = rating
+                return new_movie
+            else:
+                return None
+    else:
+        return None
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
