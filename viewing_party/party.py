1552bd973ba80569e05bd5a9e6a3c531c6e46de8
Maria Silva <hello@mariasilva.dev>
2022-09-26 21:31:41 -0500

    wave 1 completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,17 +1,20 @@
 # ------------- WAVE 1 --------------------
 def create_movie(title, genre, rating):
     new_movie = {}
-    if len(title) > 0:
+    if title != None:
         new_movie["title"] = title
-        if len(genre) > 0:
+        if genre != None:
             new_movie["genre"] = genre
-            if rating:
+            if rating != None:
                 new_movie["rating"] = rating
                 return new_movie
             else:
                 return None
+        else:
+            return None
     else:
         return None
+            
 
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
@@ -20,10 +23,22 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
+
+def watch_movie(user_data, title):
+    for i in range(len(user_data["watchlist"])):
+        if title == user_data["watchlist"][i]["title"]: 
+            user_data["watched"].append(user_data["watchlist"][i])
+            user_data["watchlist"].pop(i)
+            
+            return user_data
+        # else:
+    return user_data
+    
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
@@ -37,4 +52,3 @@ def add_to_watchlist(user_data, movie):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
