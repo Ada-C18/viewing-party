837c378918a276800ed58d41f9492197d6007de1
Lynn <lynn@jansheski.com>
2022-10-12 09:41:24 -0700

    added asserts to test_moves_movie_from_watchlist_to_watched to include genre and rating, also asserted movie to watch in watched list.

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -153,7 +153,10 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
+    assert movie_to_watch in updated_data["watched"]
     assert updated_data["watched"][1]["title"] == movie_to_watch["title"]
+    assert updated_data["watched"][1]["genre"] == movie_to_watch["genre"]
+    assert updated_data["watched"][1]["rating"] == movie_to_watch["rating"]
     
     # raise Exception("Test needs to be completed.")
     # *******************************************************************************************
