61a8333f9980d4bc21a3dc2f35c0f92aaade5fba
Lindsey <lindseymarcea@icloud.com>
2022-10-12 21:55:30 -0400

    Edited Wave 1 code to pass 2 missing tests and modified tests and additional code

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,17 +13,21 @@ def create_movie(title, genre, rating):
     else:
         return None
 
-def add_to_watched(user_data, movie):
-    watched_movies = []
+# def add_to_watched(user_data, movie):
+#     watched_movies = []
 
-    user_data.append(movie)
+#     user_data.append(movie)
+
+#     return user_data
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
 
     return user_data
 
-def add_to_watched_list(user_data, movie):
-    watchlist = []
+def add_to_watchlist(user_data, movie):
 
-    user_data.append(movie)
+    user_data["watchlist"].append(movie)
 
     return user_data
 
@@ -36,6 +40,13 @@ def watch_movie(user_data, title):
             updated_data["watchlist"].remove(movie)
     return updated_data
 
+def watch_movie(user_data, title):
+
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+    return user_data
 
 # make a dictionary with these keys above
 #title:title, genre:genre, rating:rating
@@ -78,33 +89,28 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    unique_movies = []
+    friends_watched = [] 
 
-# def get_unique_watched(user_data):
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched.append(movie)
 
+    for movie in user_data["watched"]:
+        if movie not in friends_watched:
+            unique_movies.append(movie)
+    return unique_movies
 
-#     watched = user_data["watched"]
-#     friends = user_data["friends"]
-#     unique_movies = []
-#     # movies_user_watched = watched(user_data)
-#     # movies_friend_watched = friends(user_data)
+def get_friends_unique_watched(user_data):
+    friends_unique_movies = []
 
-
-#     watched.set(watched)
-#     friends.set(friends)
-
-#     for movie in watched:
-#         if movie == (movie["title"], movie["genre"]):
-#             unique_movies.append(movie)
-
-#         return unique_movies
-
-# def get_unique_friends_watched(user_data):
-    # friends_movies = list_friends_movies(user_data)
-    # user_not_watched = []
-    # for not_watched in friends_movies:
-    #     if not_watched not in user_data["watched"] and not_watched not in user_not_watched:
-    #         user_not_watched.append(not_watched)
-    #     return user_not_watched
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in user_data["watched"] and movie not in friends_unique_movies:
+                friends_unique_movies.append(movie)
+    return friends_unique_movies
+ 
 
 
 
@@ -129,11 +135,27 @@ def get_available_recs(user_data):
 # -----------------------------------------
 
 
-# def get_new_rec_by_genre(user_data):
+def get_new_rec_by_genre(user_data):
+    recommended = []
 
+    favorite_genre = get_most_watched_genre(user_data)
 
+    for friend in user_data["friend"]:
+        for movie in friend["watched"]:
+            if movie not in user_data["watched"] and movie["genre"] == favorite_genre:
+                recommended.append(movie)
+    return recommended
+    #removed exception from test 3 to pass wave 3, fixed attribute error in wave 1 for it to pass and fixed key error in wave 5 for it to pass
 
-    
+def get_rec_from_favorites(user_data):
+    friends_list = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_list.append(movie["title"])
 
-# def get_rec_from_favorites(user_data):
+    recommended_movies = []
+    for movie in user_data["favorites"]:
+        if movie["title"] not in friends_list:
+            recommended_movies.append(movie)
+    return recommended_movies
 
