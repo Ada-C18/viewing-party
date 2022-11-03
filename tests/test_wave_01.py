9d102499950273499101dcf6fd3921699369a43e
enigmatic-loop <113566200+enigmatic-loop@users.noreply.github.com>
2022-11-03 10:39:44 -0700

    Update tests/test_wave_01.py
    Co-authored-by: jbieniosek <jbieniosek@gmail.com>

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -120,7 +120,9 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watched"]) == 1
     
     #raise Exception("Test needs to be completed.")
-    assert janes_data["watched"][-1]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
